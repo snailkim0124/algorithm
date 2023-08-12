7cb61b58d90eaf8c8175b5ae82170fc7731ca59e
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-08-12 11:28:43 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+int n, m;
+int res[10];
+int arr[10];
+int check[10];
+
+void dfs(int depth){
+	if (depth == m) {
+		for (int i = 0; i < m; i++){
+			cout << res[i] << " ";
+		}
+		cout << "\n";
+	}
+
+	else {
+		for (int i = 0; i < n; i++) {
+			if (check[i] == 0) {
+				res[depth] = arr[i];
+				check[i] = 1;
+				dfs(depth + 1);
+				check[i] = 0;
+			}
+		}
+	}
+
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	dfs(0);
+	return 0;
+}
