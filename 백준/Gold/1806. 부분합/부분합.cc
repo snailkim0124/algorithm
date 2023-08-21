0d3820704925165615208473dc2276cd8d7f84a6
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-08-22 00:22:27 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int n, s, MIN = 2000000001;
+int a[100005];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	int left = 0;
+	int right = 0;
+	int sum = a[0];
+
+	while (left <= right && right < n) {
+		if (a[left] == s || a[right] == s) {
+			cout << "1";
+			exit(0);
+		}
+
+		if (sum >= s) {
+			if (right - left + 1 < MIN) {
+				MIN = right - left +1;
+			}
+			sum -= a[left];
+			left++;
+		}
+		else {
+			right++;
+			sum += a[right];
+		}
+	}
+	if (MIN == 2000000001) {
+		cout << "0";
+	}
+	else {
+		cout << MIN;
+	}
+
+	return 0;
+}
