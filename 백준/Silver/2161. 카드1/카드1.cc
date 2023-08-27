d79b1a5e383aa82d0dc36277ebb7bc357199a700
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-08-27 19:18:51 +0900

    [Silver V] Title: 카드1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+int n;
+queue<int> q;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	while (q.size() > 1) {
+		cout << q.front() << " ";
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front() << " ";
+	return 0;
+}
