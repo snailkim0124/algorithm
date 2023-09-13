e23a282fbd1fc0b8f554ded55e1839c885f590b7
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-09-13 12:40:13 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <math.h>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	cout << n - 1946;
+	return 0;
+}
