2eaff6afca9d7fb8af79e65ad49adb6927c5e1d3
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-09-15 14:01:33 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <math.h>
+#include <string>
+#include <memory.h>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cout << 377 << "\n";
+	cout << "kimmingim";
+	return 0;
+}
