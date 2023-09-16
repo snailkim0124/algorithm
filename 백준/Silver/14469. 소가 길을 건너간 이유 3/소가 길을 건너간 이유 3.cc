7c76e428010f267950b86e27480fbbb26643dfd2
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-09-16 13:49:01 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <tuple>
+#include <memory.h>
+
+using namespace std;
+
+int n;
+vector<pair<int, int>> v;
+int res;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int start, timing;
+		cin >> start >> timing;
+		v.push_back({ start, timing });
+	}
+	
+	sort(v.begin(), v.end());
+
+	res += v[0].first + v[0].second;
+
+	for (int i = 1; i < n; i++) {
+		if (res <= v[i].first) {
+			res += v[i].first - res;
+			res += v[i].second;
+		}
+		else {
+			res += v[i].second;
+		}
+	}
+
+	cout << res;
+	
+	return 0;
+}
