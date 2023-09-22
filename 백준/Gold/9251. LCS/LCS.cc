317a735e8a9b18aaaafdff66842248c791425fdc
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-09-22 22:31:31 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5972 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <string>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <tuple>
+
+using namespace std;
+
+string s1, s2;
+int dp[1005][1005];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> s1;
+	cin >> s2;
+
+	for (int i = 1; i <= s1.size(); i++) {
+		for (int j = 1; j <= s2.size(); j++) {
+			if (s1[i - 1] == s2[j - 1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+
+	cout << dp[s1.size()][s2.size()];
+	return 0;
+}
