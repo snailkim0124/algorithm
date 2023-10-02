c2b89dc5936979ebe4c52a992f0369018e387371
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-10-02 22:59:43 +0900

    [Gold V] Title: 최소비용 구하기, Time: 36 ms, Memory: 4744 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int dist[1005]; // 가중치
+const int INF = 987654321;
+vector<pair<int, int>> adj[1005];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+int start, _end;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	fill(dist, dist + 1005, INF); // 무한대 값으로 초기화
+	cin >> N;
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		adj[u].push_back(make_pair(w, v));
+	}
+	cin >> start >> _end; // 시작, 끝
+
+	pq.push({ 0, start });
+	dist[start] = 0;
+
+	while (!pq.empty()) {
+		int here = pq.top().second;
+		int here_dist = pq.top().first;
+		pq.pop();
+		if (dist[here] != here_dist) continue;
+		for (pair<int, int> there : adj[here]) {
+			int _dist = there.first;
+			int _there = there.second;
+			if (dist[_there] > dist[here] + _dist) {
+				dist[_there] = dist[here] + _dist;
+				pq.push(make_pair(dist[_there], _there));
+			}
+		}
+	}
+	
+	cout << dist[_end];
+
+	return 0;
+}
