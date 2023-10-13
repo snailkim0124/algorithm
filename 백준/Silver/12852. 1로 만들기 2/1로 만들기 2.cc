b82a9019f3e9d8961c4d90e2398fe563f088124b
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-10-14 01:08:11 +0900

    [Silver I] Title: 1로 만들기 2, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -1,46 +1,35 @@
-#include <iostream>
-#include <algorithm>
-#include <stack>
-#include <utility>
-#include <vector>
-#include <cmath>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
 
-int arr[1000004];
-int dp[1000004];
 int n;
-vector<int> v;
+int dp[1000005];
+const int INF = 987654321;
+
+void trace(int n) {
+	if (n == 0) return;
+	cout << n << " ";
+	if (n % 3 == 0 && dp[n] == (dp[n / 3] + 1)) trace(n / 3);
+	else if (n % 2 == 0 && dp[n] == (dp[n / 2] + 1)) trace(n / 2);
+	else if ((n - 1 >= 0) && (dp[n] == (dp[n - 1] + 1))) trace(n - 1);
+	return;
+}
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n;
+	cin >> n;
+	fill(dp, dp + 1000005, INF);
+	dp[1] = 0;
+	for (int i = 1; i <= n; i++) {
+		if (!(i % 3)) dp[i] = min(dp[i / 3] + 1, dp[i]);
+		if (!(i % 2)) dp[i] = min(dp[i / 2] + 1, dp[i]);
+		dp[i] = min(dp[i - 1] + 1, dp[i]);
+	}
+	cout << dp[n] << "\n";
+	trace(n);
 
-    for (int i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + 1;
-        arr[i] = i - 1;
-        if (i % 2 == 0) {
-            if (dp[i] > dp[i / 2] + 1) {
-                dp[i] = dp[i / 2] + 1;
-                arr[i] = i / 2;
-            }
-        }
-        if (i % 3 == 0) {
-            if (dp[i] > dp[i / 3] + 1) {
-                dp[i] = dp[i / 3] + 1;
-                arr[i] = i / 3;
-            }
-        }
-    }
-
-    cout << dp[n] << "\n";
-    for (int i = n; i != 1; i = arr[i]) {
-        v.push_back(i);
-    }
-    v.push_back(1);
-    for (auto it : v) cout << it << " ";
-    return 0;
+	return 0;
 }
