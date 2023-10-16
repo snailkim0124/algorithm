137bf59ba6cc59c91050e85b3dd528e91b6dfbd4
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-10-16 23:00:54 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	if (n) cout << "Leading the Way to the Future";
+	else cout << "YONSEI";
+	return 0;
+}
