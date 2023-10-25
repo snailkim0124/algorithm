06b7c398f811b05d600d4e4d1080ad7996bbb455
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-10-25 22:36:20 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int r1, s;
+int r2;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> r1 >> s;
+	r2 = s * 2 - r1;
+	cout << r2;
+	return 0;
+}
