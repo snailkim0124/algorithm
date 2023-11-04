3e639e7a3c5e7b60f1113bbbdea73cbd86e7d9b3
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-11-05 02:29:13 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	cout << 2000 << "\n";
+	for (int i = 1; i <= 1000; i++) {
+		cout << 1 << " ";
+	}
+	for (int i = 1; i <= 1000; i++) {
+		cout << 1000 << " ";
+	}
+	
+	return 0;
+}
