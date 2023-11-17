080ad1c6bd86f5ad66dc7e40223ef0dc0c2d4021
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-11-18 01:24:21 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int n;
+int arr[100005];
+stack<int> st;
+int res;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+
+	st.push(0);
+	for (int i = 1; i <= n + 1; i++) {
+		while (!st.empty() && arr[st.top()] > arr[i]) {
+			int tmp = st.top();
+			st.pop();
+			res = max(res, arr[tmp] * (i - st.top() - 1));
+		}
+		st.push(i);
+	}
+
+	cout << res;
+
+	return 0;
+}
