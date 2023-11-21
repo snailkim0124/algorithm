e49db20d94f70d6ce16cbbbae715f94b261855e5
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-11-21 22:05:58 +0900

    [Gold V] Title: 호반우가 학교에 지각한 이유 3, Time: 120 ms, Memory: 10420 KB -BaekjoonHub

---
+++ 백준/Gold/30470. 호반우가 학교에 지각한 이유 3/호반우가 학교에 지각한 이유 3.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int n;
+stack<pair<ll, ll>> st; // (길이, 개수)
+ll cnt;
+ll res;
+ll mx;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	st.push({ 0,0 });
+	for (int i = 0; i < n; i++) {
+		ll a, b;
+		cin >> a >> b;
+		if (a == 1) {
+			st.push({ b, 1 });
+			mx = max(mx, b);
+		}
+		else if (a == 2) {
+			ll sum = 0;
+			while (!st.empty() && st.top().first >= max(0LL, mx - b)) {
+				sum += st.top().second;
+				st.pop();
+			}
+			if (sum) st.push({ max(0LL, mx - b), sum });
+			if (!st.empty()) mx = st.top().first;
+		}
+	}
+
+	while (!st.empty()) {
+		//cout << st.top().first << " " << st.top().second << "\n";
+		res += st.top().first * st.top().second;
+		st.pop();
+	}
+
+	cout << res;
+
+	return 0;
+}
