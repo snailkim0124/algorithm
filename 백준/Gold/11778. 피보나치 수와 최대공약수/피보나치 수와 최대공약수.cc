0e113b8ad9f5907005852a53ac9db662b487491a
minhwan <128965735+snailkim0124@users.noreply.github.com>
2023-11-29 02:01:44 +0900

    [Gold I] Title: 피보나치 수와 최대공약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11778. 피보나치 수와 최대공약수/피보나치 수와 최대공약수.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll arr[2][2], ans[2][2], tmp[2][2];
+ll n, m;
+const int mod = 1000000007;
+
+ll gcd(ll a, ll b) {
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+
+void matrix_multi(ll a[][2], ll b[][2]) {
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			tmp[i][j] = 0;
+			for (int k = 0; k < 2; k++) {
+				tmp[i][j] += (a[i][k] * b[k][j]);
+			}
+			tmp[i][j] %= mod;
+		}
+	}
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			a[i][j] = tmp[i][j];
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < 2; i++) { // 단위행렬 만들기
+		ans[i][i] = 1;
+	}
+	arr[0][0] = 1;
+	arr[0][1] = 1;
+	arr[1][0] = 1;
+
+	ll GCD = gcd(n, m);
+
+	if (GCD == 0) {
+		cout << 0;
+		exit(0);
+	}
+
+	while (GCD > 0) { // 행렬 곱셈 이용
+		if (GCD % 2 == 1) {
+			matrix_multi(ans, arr);
+		}
+		matrix_multi(arr, arr);
+		GCD /= 2;
+	}
+
+	cout << ans[0][1] % mod;
+
+	return 0;
+}
