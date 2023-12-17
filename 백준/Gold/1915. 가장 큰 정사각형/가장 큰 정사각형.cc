b7ca0f93f68b695571e12f6a02373521bec9e2e0
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2023-12-17 14:22:35 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 80 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int n, m;
+int arr[1005][1005];
+int dp[1005][1005];
+int mx = 0;
+
+int main() {
+	//ios_base::sync_with_stdio(false);
+	//cin.tie(NULL); cout.tie(NULL);
+
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			scanf("%1d", &arr[i][j]);
+		}
+	}
+
+	if (n == 1 && m == 1) {
+		printf("%d", arr[1][1]);
+		exit(0);
+	}
+
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (arr[i][j] == 1) {
+				arr[i][j] = min(arr[i-1][j-1], min(arr[i][j - 1], arr[i - 1][j])) + 1;
+			}
+			mx = max(mx, arr[i][j]);
+		}
+	}
+
+	printf("%d", mx * mx);
+
+	return 0;
+}
