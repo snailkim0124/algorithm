730831620ffc144c414440c21e999d563948ab7e
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2023-12-21 15:34:40 +0900

    [Silver II] Title: 제곱수의 합, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int n;
+int dp[100005];
+const int INF = 987654321;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	dp[1] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		int mn = INF;
+		for (int j = 1; j * j <= i; j++) {
+			int tmp = i - j * j;
+			mn = min(mn, dp[tmp]);
+		}
+		dp[i] = mn + 1;
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
