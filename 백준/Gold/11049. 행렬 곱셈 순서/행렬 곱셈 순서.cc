113d26e18435c2072d107ada1f170c7aa9c98052
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2023-12-24 01:35:58 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 40 ms, Memory: 3020 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -8,28 +8,6 @@ int dp[505][505];
 const int INF = 987654321;
 pair<int, int> v[505];
 
-int go(int x, int y) {
-	if (dp[x][y] != INF) { // 값 바뀐 경우
-		return dp[x][y];
-	}
-
-	if (x == y) { // 같은 경우
-		return 0;
-	}
-	if (x + 1 == y) { // 행렬 계산
-		return v[x].first * v[x].second * v[y].second;
-	}
-
-	int& res = dp[x][y];
-
-	for (int i = x; i < y; i++) {
-		int tmp = v[x].first * v[i].second * v[y].second + go(x, i) + go(i + 1, y);
-		res = min(res, tmp);
-	}
-
-	return res;
-}
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -42,9 +20,20 @@ int main() {
 	}
 
 
-	fill(&dp[0][0], &dp[0][0] + 505 * 505, INF); // 최대로 초기화
+	//fill(&dp[0][0], &dp[0][0] + 505 * 505, INF); // 최대로 초기화
 
-	cout << go(1, n);
+	// 3중 for문
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; i + j <= n; j++) {
+			dp[j][i + j] = INF;
+			for (int k = j; k <= i + j; k++) {
+				int tmp = v[j].first * v[k].second * v[i + j].second + dp[j][k] + dp[k + 1][i + j];
+				dp[j][i + j] = min(dp[j][i + j], tmp);
+			}
+		}
+	}
+
+	cout << dp[1][n];
 
 	return 0;
 }
