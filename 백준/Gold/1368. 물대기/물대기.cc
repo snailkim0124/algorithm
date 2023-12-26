5f63c6ee16f726edf7a4a2507841dec421e075ca
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2023-12-26 17:09:01 +0900

    [Gold II] Title: 물대기, Time: 12 ms, Memory: 3668 KB -BaekjoonHub

---
+++ 백준/Gold/1368. 물대기/물대기.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+typedef struct EDGE {
+	int s, e, v;
+	bool operator > (const EDGE& temp) const {
+		return v > temp.v;
+	}
+}EDGE;
+
+int n, m;
+int parent[305];
+priority_queue<EDGE, vector<EDGE>, greater<EDGE>> pq; // 오름차순
+int arr[305][305];
+
+int findParent(int a) {
+	if (a == parent[a]) {
+		return a;
+	}
+	else {
+		return parent[a] = findParent(parent[a]);
+	}
+}
+
+void unionParent(int a, int b) {
+	a = findParent(a);
+	b = findParent(b);
+
+	if (a != b) {
+		parent[b] = a;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int well;
+		cin >> well;
+		pq.push({ 0, i, well });
+	}
+	for (int i = 1; i <= n; i++) { // 부모 초기화
+		parent[i] = i;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = i + 1; j <= n; j++) {
+			pq.push({ i, j, arr[i][j] });
+		}
+	}
+
+	int res = 0;
+
+	while (!pq.empty()) {
+		EDGE now = pq.top();
+		pq.pop();
+
+		if (findParent(now.s) != findParent(now.e)) { // 연결핻도 사이클이 생기지 않는다면
+			unionParent(now.s, now.e);
+			res += now.v;
+		}
+	}
+
+	cout << res;
+	return 0;
+}
