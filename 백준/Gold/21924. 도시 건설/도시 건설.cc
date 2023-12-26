a05872d5e7fcb9b92681bc47715adc0b7c0f304d
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2023-12-26 17:14:05 +0900

    [Gold IV] Title: 도시 건설, Time: 304 ms, Memory: 20976 KB -BaekjoonHub

---
+++ 백준/Gold/21924. 도시 건설/도시 건설.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+typedef struct EDGE {
+	ll s, e, v;
+	bool operator > (const EDGE& temp) const {
+		return v > temp.v;
+	}
+}EDGE;
+
+int n, m;
+int parent[100005];
+priority_queue<EDGE, vector<EDGE>, greater<EDGE>> pq; // 오름차순
+ll sum;
+
+int findParent(ll a) {
+	if (a == parent[a]) {
+		return a;
+	}
+	else {
+		return parent[a] = findParent(parent[a]);
+	}
+}
+
+void unionParent(ll a, ll b) {
+	a = findParent(a);
+	b = findParent(b);
+
+	if (a != b) {
+		parent[b] = a;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) { // 부모 초기화
+		parent[i] = i;
+	}
+
+	for (int i = 0; i < m; i++) {
+		ll a, b, c;
+		cin >> a >> b >> c;
+		pq.push({ a,b,c });
+		sum += c;
+	}
+
+	int useEdge = 0;
+	ll res = 0;
+
+	while (!pq.empty()) {
+		EDGE now = pq.top();
+		pq.pop();
+
+		if (findParent(now.s) != findParent(now.e)) { // 연결핻도 사이클이 생기지 않는다면
+			unionParent(now.s, now.e);
+			res += now.v;
+			useEdge++;
+		}
+	}
+
+	if (useEdge == n - 1) cout << sum - res;
+	else cout << -1;
+
+	return 0;
+}
