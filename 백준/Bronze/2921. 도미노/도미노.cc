2ea3f42f660f4e6dbe721eba4defe81a1a37e540
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2024-01-13 19:32:24 +0900

    [Bronze III] Title: 도미노, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	cout << n * (n + 1) * (n + 2) / 2;
+	
+
+	return 0;
+}
