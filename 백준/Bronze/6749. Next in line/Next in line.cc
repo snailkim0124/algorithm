e19305e4e5fc8d73bb7755065aab47b7acd1c57d
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2024-01-15 00:32:26 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int x, y;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> x;
+	cin >> y;
+	cout << y + y - x;
+	
+	return 0;
+}
