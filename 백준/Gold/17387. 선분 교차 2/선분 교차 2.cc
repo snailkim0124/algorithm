8c5f7aad4ef2802c7444938cb168531d113d9673
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2024-01-16 14:06:55 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+ll ccw(pll p1, pll p2, pll p3) {
+	ll s = p1.first * p2.second + p2.first * p3.second + p3.first * p1.second;
+	s -= (p1.second * p2.first + p2.second * p3.first + p3.second * p1.first);
+
+	if (s > 0) return 1;
+	else if (s == 0) return 0;
+	else return -1;
+}
+
+bool iscross(pair<pll, pll> l1, pair<pll, pll> l2) {
+	pll p1 = l1.first;
+	pll p2 = l1.second;
+	pll p3 = l2.first;
+	pll p4 = l2.second;
+
+	ll p1p2 = ccw(p1, p2, p3) * ccw(p1, p2, p4);
+	ll p3p4 = ccw(p3, p4, p1) * ccw(p3, p4, p2);
+
+	if (p1p2 == 0 && p3p4 == 0) {
+		if (p1 > p2) swap(p2, p1);
+		if (p3 > p4) swap(p3, p4);
+
+		return p3 <= p2 && p1 <= p4;
+	}
+
+	return p1p2 <= 0 && p3p4 <= 0;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	pll p1, p2, p3, p4;
+	cin >> p1.first >> p1.second >> p2.first >> p2.second;
+	cin >> p3.first >> p3.second >> p4.first >> p4.second;
+
+	if (iscross({ p1,p2 }, { p3,p4 })) {
+		cout << 1;
+	}
+	else cout << 0;
+
+
+	return 0;
+}
