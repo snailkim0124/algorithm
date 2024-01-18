c9f23394208de6f589cb53fd3f6f34a57109093f
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2024-01-18 14:12:07 +0900

    [Gold I] Title: 조합 (Combination), Time: 8 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int n, k;
+const int mod = 1000000007;
+ll tmp;
+ll ans;
+
+ll divide_pow(ll n, ll k) {
+	ll res = 1;
+
+	while (k) {
+		if (k % 2 == 1) res = (res * n) % mod;
+		n = (n * n) % mod;
+		k /= 2;
+	}
+	return res;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> k;
+
+	ll a = 1;
+	ll b = 1;
+	// 페르마의 소정리
+	for (int i = n; i >= n - k + 1; i--) {
+		a = (a * i) % mod;
+	}
+	for (int i = 1; i <= k; i++) {
+		b = (b * i) % mod;
+	}
+	ans = ((a % mod) * divide_pow(b, mod - 2) % mod) % mod;
+
+	cout << ans;
+	return 0;
+}
