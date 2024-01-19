8d0d33173fb788957143e82d754453f2832269a7
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2024-01-19 21:39:46 +0900

    [Bronze II] Title: 부분 문자열, Time: 16 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
@@ -3,20 +3,36 @@
 using namespace std;
 typedef long long ll;
 
-char s[1000005], p[1000005];
+vector<int> failure(string& s) {
+	vector<int> f(s.size());
+	int j = 0;
+	for (int i = 1; i < s.size(); i++) {
+		while (j > 0 && s[i] != s[j]) j = f[j - 1];
+		if (s[i] == s[j]) f[i] = ++j;
+	}
+
+	return f;
+}
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> s;
-	cin >> p;
+	string s, p;
+	cin >> s >> p;
+	vector<int> f = failure(p);
 
-	if (strstr(s, p) != NULL) {
-		cout << 1;
+	int j = 0;
+	for (int i = 0; i < s.size(); i++) {
+		while (j > 0 && s[i] != p[j]) j = f[j - 1];
+		if (s[i] == p[j]) j++;
+		if (j == p.size()) {
+			cout << 1;
+			exit(0);
+		}
 	}
-	else cout << 0;
 
-
+	cout << 0;
+	
 	return 0;
 }
