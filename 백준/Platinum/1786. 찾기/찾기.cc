31ec50f57a9f8bfca3ef813dc6e52ffeeca5c825
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2024-01-19 23:11:00 +0900

    [Platinum V] Title: 찾기, Time: 60 ms, Memory: 9432 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int cnt = 0;
+vector<int> v;
+
+vector<int> failure(string& s) {
+	vector<int> f(s.size());
+	int j = 0;
+	for (int i = 1; i < s.size(); i++) {
+		while (j > 0 && s[i] != s[j]) j = f[j - 1];
+		if (s[i] == s[j]) f[i] = ++j;
+	}
+
+	return f;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string t, p;
+	getline(cin, t);
+	getline(cin, p);
+	vector<int> f = failure(p);
+
+	int j = 0;
+	for (int i = 0; i < t.size(); i++) {
+		while (j > 0 && t[i] != p[j]) j = f[j - 1];
+		if (t[i] == p[j]) j++;
+		if (j == p.size()) {
+			cnt++;
+			v.push_back(i - (p.size() - 2));
+		}
+	}
+
+	cout << cnt << "\n";
+	for (auto it : v) {
+		cout << it << " ";
+	}
+
+	return 0;
+}
