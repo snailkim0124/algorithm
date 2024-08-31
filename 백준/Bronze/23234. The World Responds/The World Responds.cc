a0f5f4dd1c2004aae39f47002e541ef2fc818e0b
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2024-08-31 14:50:34 +0900

    [Bronze V] Title: The World Responds, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cout << "The world says hello!";
+
+	return 0;
+}
