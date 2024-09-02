62c8d35c34f7c1f4e5c5f05c0877fbd51cda866f
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2024-09-02 15:57:48 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int n;
+int dp[1005];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	dp[2] = 1;
+	dp[4] = 1;
+
+	cin >> n;
+
+	for (int i = 5; i <= n; i++) {
+		if (!dp[i - 1] || !dp[i - 3] || !dp[i - 4]) dp[i] = 1;
+	}
+
+	if (dp[n]) cout << "SK";
+	else cout << "CY";
+
+	return 0;
+}
