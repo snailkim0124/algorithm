6edcae667e1dcef6c717f70feff4e90be17687f5
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-05-09 05:35:39 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+ll n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> n;
+    cout << n * n * n;
+
+
+    return 0;
+}
