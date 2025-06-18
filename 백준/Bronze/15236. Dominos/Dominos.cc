13d73537f1bc8e7bf8700ef8993305ff699e085b
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-06-18 19:51:21 +0900

    [Bronze III] Title: Dominos, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15236. Dominos/Dominos.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
+	cout << n * (n + 1) * (n + 2) / 2;
+	
+	return 0;
+}
