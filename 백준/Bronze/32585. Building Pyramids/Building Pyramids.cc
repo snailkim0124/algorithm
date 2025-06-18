eefc423115dadc00cdc0d102195009d837284419
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-06-18 20:52:16 +0900

    [Bronze III] Title: Building Pyramids, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/32585. Building Pyramids/Building Pyramids.cc
+++ 백준/Bronze/32585. Building Pyramids/Building Pyramids.cc
@@ -6,19 +6,13 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 ll n;
-ll res;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n;
-	
-	for (ll i = 1; i <= n; i++) {
-		res += i * (i + 1) / 2;
-	}
-	
-	cout << res;
+	cout << n * (n + 1) * (n + 2) / 6;
 	
 	return 0;
 }
