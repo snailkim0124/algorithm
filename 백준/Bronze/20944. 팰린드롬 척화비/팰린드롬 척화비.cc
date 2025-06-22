7e54ac171e5a4941a1925b8b8e0f270186fda49a
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-06-22 22:34:52 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) cout << "a";
+
+
+	return 0;
+}
