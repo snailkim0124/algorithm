5a84a3a463c763527d40669baee0af8a514b0e68
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-06-26 15:22:51 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2040 KB, Score: 12 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -1,36 +1,41 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 
 int n, k;
-vector<pair<tuple<int, int, int>, int>> v;
+typedef struct medal {
+	int num;
+	int gold, silver, copper;
+} medal;
+medal arr[1005];
 
+bool cmp(medal& a, medal& b) {
+	if (a.gold != b.gold) return a.gold > b.gold;
+	if (a.silver != b.silver) return a.silver > b.silver;
+	if (a.copper != b.copper) return a.copper > b.copper;
+	return a.num < b.num;
+}
 
 int main() {
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n >> k;
 	for (int i = 0; i < n; i++) {
-		int num, a, b, c;
-		cin >> num >> a >> b >> c;
-		v.push_back({ { a,b,c }, num });
+		cin >> arr[i].num >> arr[i].gold >> arr[i].silver >> arr[i].copper;
 	}
 
-	sort(v.begin(), v.end());
+	sort(arr, arr + n, cmp);
 
 	for (int i = 0; i < n; i++) {
-		if (v[i].second == k) {
-			if (i != 0 && v[i].first == v[i - 1].first) {
-				cout << i;
-			}
-			else {
-				cout << i + 1;
-			}
+		if (arr[i].num == k) {
+			cout << i + 1;
 			break;
 		}
 	}
-	
+
 	return 0;
 }
