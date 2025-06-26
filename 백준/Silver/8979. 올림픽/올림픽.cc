3d857e601a0b72c3d57543e1bdc38fb1e627cce2
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-06-26 15:37:07 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2040 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -8,14 +8,15 @@ typedef pair<ll, ll> pll;
 int n, k;
 typedef struct medal {
 	int num;
-	int gold, silver, copper;
+	int gold, silver, bronze;
 } medal;
 medal arr[1005];
+int cnt = 1, same = 0;
 
 bool cmp(medal& a, medal& b) {
 	if (a.gold != b.gold) return a.gold > b.gold;
 	if (a.silver != b.silver) return a.silver > b.silver;
-	if (a.copper != b.copper) return a.copper > b.copper;
+	if (a.bronze != b.bronze) return a.bronze > b.bronze;
 	return a.num < b.num;
 }
 
@@ -25,16 +26,29 @@ int main() {
 
 	cin >> n >> k;
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i].num >> arr[i].gold >> arr[i].silver >> arr[i].copper;
+		cin >> arr[i].num >> arr[i].gold >> arr[i].silver >> arr[i].bronze;
 	}
 
 	sort(arr, arr + n, cmp);
 
+
+	/*cout << "\n";
 	for (int i = 0; i < n; i++) {
+		cout << arr[i].num << " " << arr[i].gold << " " << arr[i].silver << " " << arr[i].bronze << "\n";
+	}*/
+
+	for (int i = 0; i < n; i++) {
+		if (i > 0 && (arr[i].gold != arr[i - 1].gold || arr[i].silver != arr[i - 1].silver || arr[i].bronze != arr[i - 1].bronze)) {
+			cnt += same;
+			same = 0;
+		}
+
 		if (arr[i].num == k) {
-			cout << i + 1;
+			cout << cnt;
 			break;
 		}
+
+		same++;
 	}
 
 	return 0;
