d91401b9b376ac92aba56b4fbc1c1735e9b9ff11
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-06-29 22:22:02 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int n;
+int arr[1005];
+int dp[1005];
+int res = -1;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		dp[i]++;
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		res = max(res, dp[i]);
+	}
+
+	cout << res;
+	
+	return 0;
+}
