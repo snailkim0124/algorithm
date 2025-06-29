caa199b57360ea4879443389d7524cde60bd1743
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-06-29 20:05:58 +0900

    [Silver I] Title: 점프, Time: 0 ms, Memory: 2192 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.cc
+++ 백준/Silver/1890. 점프/점프.cc
@@ -9,22 +9,6 @@ ll n;
 ll arr[105][105];
 ll dp[105][105];
 
-ll go(int y, int x) {
-	ll& res = dp[y][x];
-	if (y == n && x == n) return 1;
-	if (res != -1) return res;
-
-	res = 0;
-	if (y + arr[y][x] <= n) {
-		res += go(y + arr[y][x], x);
-	}
-	if (x + arr[y][x] <= n) {
-		res += go(y, x + arr[y][x]);
-	}
-
-	return res;
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -36,9 +20,15 @@ int main() {
 		}
 	}
 
-	memset(dp, -1, sizeof(dp));
-
-	cout << go(1, 1) << "\n";
+	dp[1][1] = 1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (i == n && j == n) break;
+			if (i + arr[i][j] <= n) dp[i + arr[i][j]][j] += dp[i][j];
+			if (j + arr[i][j] <= n) dp[i][j + arr[i][j]] += dp[i][j];
+		}
+	}
+	cout << dp[n][n] << "\n";
 
 	return 0;
 }
