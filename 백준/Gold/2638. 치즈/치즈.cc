200ed8d6f7c58afa50a448d6c9283e3bb536bd74
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-07-05 14:55:01 +0900

    [Gold III] Title: 치즈, Time: 8 ms, Memory: 2108 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int n, m;
+int arr[105][105];
+int visited[105][105];
+const int dy[4] = { -1,0,1,0 };
+const int dx[4] = { 0,1,0,-1 };
+int cnt;
+
+int bfs() {
+	bool isMelted = false;
+	queue<pii> q;
+	visited[0][0] = 1;
+	q.push({ 0 ,0 });
+
+	while (!q.empty()) {
+		int y, x;
+		tie(y, x) = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+			if (arr[ny][nx] == 1) {
+				visited[ny][nx]++;
+				if (visited[ny][nx] >= 2) {
+					arr[ny][nx] = 0;
+				}
+				isMelted = true;
+			}
+			else if(arr[ny][nx] == 0 && !visited[ny][nx]) {
+				visited[ny][nx] = 1;
+				q.push({ ny, nx });
+			}
+		}
+	}
+
+	return isMelted;
+}
+
+void printCheese() {
+	cout << "============================\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "============================\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	while (1) {
+		memset(visited, 0, sizeof(visited));
+		if (!bfs()) break;
+		cnt++;
+		// printCheese();
+	}
+
+	cout << cnt;
+
+	return 0;
+}
