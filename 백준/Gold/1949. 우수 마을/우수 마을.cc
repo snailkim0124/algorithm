aee28e07c442ceae9f1b71d6cbd441e13835685e
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-07-07 12:54:46 +0900

    [Gold II] Title: 우수 마을, Time: 4 ms, Memory: 3148 KB -BaekjoonHub

---
+++ 백준/Gold/1949. 우수 마을/우수 마을.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int n;
+int visited[10005];
+int arr[10005];
+int dp[10005][2]; // 선정되면 1, 선정 안되면 0
+vector<int> adj[10005];
+
+void dfs(int now) {
+	if (visited[now]) return;
+	visited[now] = 1;
+
+	dp[now][1] = arr[now];
+
+	for (auto next : adj[now]) {
+		if (!visited[next]) {
+			dfs(next);
+			dp[now][0] += max(dp[next][0], dp[next][1]);
+			dp[now][1] += dp[next][0];
+		}
+	}
+
+	return;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	dfs(1);
+
+	/*for (int i = 0; i < 2; i++) {
+		for (int j = 1; j <= n; j++) {
+			cout << dp[j][i] << " ";
+		}
+		cout << "\n";
+	}*/
+
+	cout << max(dp[1][0], dp[1][1]);
+
+	return 0;
+}
