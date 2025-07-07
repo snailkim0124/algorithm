8bc8a0685cd3dec0c4cc27e936d15f2958429bea
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-07-07 15:53:03 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int t;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> t;
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+		cout << "yes\n";
+	}
+
+	
+	return 0;
+}
