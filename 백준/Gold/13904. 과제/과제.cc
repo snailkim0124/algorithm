fb967dd8f14070faabf4ad228464e1d1e1af10df
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-07-10 20:47:16 +0900

    [Gold III] Title: 과제, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.cc
+++ 백준/Gold/13904. 과제/과제.cc
@@ -5,16 +5,12 @@ typedef unsigned long long ull;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+
 int n;
-vector<pii> v;
+priority_queue<pii, vector<pii>> pq;
 int visited[1005];
 int res;
 int mx;
-
-bool cmp(pii& a, pii& b) {
-	if (a.second == b.second) return a.first < b.first;
-	return a.second > b.second;
-}
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -24,20 +20,17 @@ int main() {
 	while (n--) {
 		int d, w;
 		cin >> d >> w;
-		v.push_back({ d, w });
+		pq.push({ w, d });
 		mx = max(mx, d);
 	}
 
-	sort(v.begin(), v.end(), cmp);
+	while (!pq.empty()) {
+		pii now = pq.top();
+		pq.pop();
 
-	//for (auto it : v) {
-	//	cout << it.first << " : " << it.second << "\n";
-	//}
-
-	for (int i = 0; i < v.size(); i++) {
-		for (int j = v[i].first; j > 0; j--) {
-			if (!visited[j]) {
-				visited[j] = v[i].second;
+		for (int i = now.second; i > 0; i--) {
+			if (!visited[i]) {
+				visited[i] = now.first;
 				break;
 			}
 		}
@@ -46,7 +39,6 @@ int main() {
 	for (int i = 1; i <= mx; i++) {
 		res += visited[i];
 	}
-
 	cout << res;
 	
 	return 0;
