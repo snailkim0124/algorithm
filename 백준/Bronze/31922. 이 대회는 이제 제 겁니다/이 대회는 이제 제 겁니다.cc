0720f1d5161c2369cf9c7c58aa4fd2e3bf2b5b09
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-07-30 21:45:18 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int a, p, c;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a >> p >> c;
+	cout << max(a + c, p);
+	
+
+	return 0;
+}
