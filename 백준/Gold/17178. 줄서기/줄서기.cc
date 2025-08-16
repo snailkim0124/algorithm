58361db3419fc0a26851c8d048252567c5388d2d
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-08-16 15:16:27 +0900

    [Gold V] Title: 줄서기, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/17178. 줄서기/줄서기.cc
+++ 백준/Gold/17178. 줄서기/줄서기.cc
@@ -6,7 +6,7 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
 int n;
-vector<pair<string, int>> v, tmp;
+vector<pair<string, int>> sorted;
 stack<pair<string, int>> st;
 queue<pair<string, int>> q;
 
@@ -15,31 +15,27 @@ int main() {
     cout.tie(NULL); cin.tie(NULL);
 
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 5; j++) {
-            string s;
-            cin >> s;
-            string c = s.substr(0, 1);
-            int num = stoi(s.substr(2));
-            q.push({ c, num });
-            v.push_back({ c, num });
-        }
+    for (int i = 0; i < n * 5; i++) {
+        string s;
+        cin >> s;
+        string c = s.substr(0, 1);
+        int num = stoi(s.substr(2));
+        q.push({ c, num });
+        sorted.push_back({ c, num });
     }
 
-    tmp = v;
-
-    sort(tmp.begin(), tmp.end());
+    sort(sorted.begin(), sorted.end());
     int idx = 0;
 
     while (!q.empty()) {
         bool check = false;
-        if (q.front() == tmp[idx]) {
+        if (q.front() == sorted[idx]) {
             q.pop();
             idx++;
             check = true;
         }
         while (!st.empty()) {
-            if (st.top() == tmp[idx]) {
+            if (st.top() == sorted[idx]) {
                 st.pop();
                 idx++;
                 check = true;
@@ -52,23 +48,6 @@ int main() {
             q.pop();
         }
     }
-
-
-    //for (auto it : tmp) {
-    //    cout << it.first << " : " << it.second << "\n";
-    //}
-
-    //cout << "q_remain : \n";
-    //while (!q.empty()) {
-    //    cout << q.front().first << " : " << q.front().second << "\n";
-    //    q.pop();
-    //}
-
-    //cout << "st_remain : \n";
-    //while (!st.empty()) {
-    //    cout << st.top().first << " : " << st.top().second << "\n";
-    //    st.pop();
-    //}
 
     cout << (st.empty() ? "GOOD" : "BAD");
 
