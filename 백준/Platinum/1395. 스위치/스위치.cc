72873c037e7ec21c22e55b963e99a111a1b07475
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-08-31 17:28:23 +0900

    [Platinum III] Title: 스위치, Time: 92 ms, Memory: 6924 KB -BaekjoonHub

---
+++ 백준/Platinum/1395. 스위치/스위치.cc
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+vector<ll> tree, lazy;
+vector<ll> v;
+
+ll init(int node, int s, int e) {
+	if (s == e) return tree[node] = v[s];
+	else {
+		int mid = (s + e) / 2;
+		return tree[node] = init(node * 2, s, mid) + init(node * 2 + 1, mid + 1, e);
+	}
+}
+
+// 느리게 갱신되는 세그트리
+void propagate(int node, int s, int e) {
+	if (lazy[node] != 0) {
+		tree[node] = (e - s + 1) - tree[node];
+		if (s != e) {
+			lazy[node * 2] ^= lazy[node];
+			lazy[node * 2 + 1] ^= lazy[node];
+		}
+		lazy[node] = 0;
+	}
+}
+
+void update(int node, int s, int e, int l, int r) {
+	propagate(node, s, e);
+
+	if (l > e || r < s) return;
+	if (l <= s && e <= r) {
+		lazy[node] ^= 1;
+		propagate(node, s, e);
+		return;
+	}
+
+	int mid = (s + e) / 2;
+	update(node * 2, s, mid, l, r);
+	update(node * 2 + 1, mid + 1, e, l, r);
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+ll query(int node, int s, int e, int l, int r) {
+	propagate(node, s, e);
+
+	if (l > e || r < s) return 0; // 범위 밖
+	if (l <= s && e <= r) return tree[node];
+
+	int mid = (s + e) / 2;
+	return query(node * 2, s, mid, l, r) + query(node * 2 + 1, mid + 1, e, l, r);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	int h = (int)ceil(log2(n));
+	int tree_size = (1 << (h + 1));
+	tree.resize(tree_size);
+	lazy.resize(tree_size);
+
+	v.resize(n + 1);
+
+	// init(1, 1, n);
+
+
+	while (m--) {
+		ll o, s, t;
+		cin >> o >> s >> t;
+		if (o == 0) {
+			update(1, 1, n, s, t);
+		}
+		else if (o == 1) {
+			cout << query(1, 1, n, s, t) << "\n";
+		}
+	}
+
+
+	return 0;
+}
