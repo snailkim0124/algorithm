e1c36019a7fb435804ce7aa5f504a9b3f9c40e51
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-08-31 18:29:10 +0900

    [Platinum II] Title: 하늘에서 떨어지는 1, 2, ..., R-L+1개의 별, Time: 116 ms, Memory: 6924 KB -BaekjoonHub

---
+++ 백준/Platinum/17353. 하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별/하늘에서 떨어지는 1， 2， ．．．， R－L＋1개의 별.cc
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+vector<ll> tree, lazy;
+vector<ll> v;
+
+ll init(int node, int s, int e) {
+	if (s == e) return tree[node] = v[s];
+	else {
+		int mid = (s + e) / 2;
+		return tree[node] = init(node * 2, s, mid) + init(node * 2 + 1, mid + 1, e);
+	}
+}
+
+// 느리게 갱신되는 세그트리
+void propagate(int node, int s, int e) {
+	if (lazy[node] != 0) {
+		tree[node] += (e - s + 1) * lazy[node];
+		if (s != e) {
+			lazy[node * 2] += lazy[node];
+			lazy[node * 2 + 1] += lazy[node];
+		}
+		lazy[node] = 0;
+	}
+}
+
+void update(int node, int s, int e, int l, int r, ll val) {
+	propagate(node, s, e);
+
+	if (l > e || r < s) return;
+	if (l <= s && e <= r) {
+		lazy[node] += val;
+		propagate(node, s, e);
+		return;
+	}
+
+	int mid = (s + e) / 2;
+	update(node * 2, s, mid, l, r, val);
+	update(node * 2 + 1, mid + 1, e, l, r, val);
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+ll query(int node, int s, int e, int l, int r) {
+	propagate(node, s, e);
+
+	if (l > e || r < s) return 0; // 범위 밖
+	if (l <= s && e <= r) return tree[node];
+
+	int mid = (s + e) / 2;
+	return query(node * 2, s, mid, l, r) + query(node * 2 + 1, mid + 1, e, l, r);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, q;
+	cin >> n;
+
+	int h = (int)ceil(log2(n));
+	int tree_size = (1 << (h + 1));
+	tree.resize(tree_size);
+	lazy.resize(tree_size);
+
+	v.resize(n + 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> v[i];
+		update(1, 1, n, i, i, v[i] - v[i - 1]);
+	}
+
+
+	cin >> q;
+
+	while (q--) {
+		ll cmd;
+		cin >> cmd;
+		if (cmd == 1) {
+			ll l, r;
+			cin >> l >> r;
+			update(1, 1, n, l, r, 1);
+			update(1, 1, n, r + 1, r + 1, -(r - l + 1));
+		}
+		else if (cmd == 2) {
+			ll x;
+			cin >> x;
+			cout << query(1, 1, n, 1, x) << "\n";
+		}
+	}
+
+
+	return 0;
+}
