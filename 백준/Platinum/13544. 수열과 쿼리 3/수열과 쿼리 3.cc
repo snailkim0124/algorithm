7674e1668324cb8768b4e7c13b6e0f38288e6ded
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-09-01 18:10:40 +0900

    [Platinum III] Title: 수열과 쿼리 3, Time: 260 ms, Memory: 29688 KB -BaekjoonHub

--- 백준/Platinum/13544. 수열과 쿼리 3/수열과 쿼리 3.cc
+++ 백준/Platinum/13544. 수열과 쿼리 3/수열과 쿼리 3.cc
@@ -3,30 +3,32 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
 typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
 
-ll arr[100005];
 vector<ll> tree[400005];
+vector<ll> v;
 
-void update(int node, int s, int e, int idx, int val) {
-	if (idx < s || idx > e) return;
-	tree[node].push_back(val);
-	if (s == idx && idx == e) return;
-	if (s != e) {
-		update(node * 2, s, (s + e) / 2, idx, val);
-		update(node * 2 + 1, (s + e) / 2 + 1, e, idx, val);
+void init(int node, int s, int e) {
+	if (s == e) tree[node].push_back(v[s]);
+	else {
+		tree[node].resize(e - s + 1);
+		int mid = (s + e) / 2;
+		init(node * 2, s, mid);
+		init(node * 2 + 1, mid + 1, e);
+		merge(tree[node * 2].begin(), tree[node *2].end(), tree[node * 2 + 1].begin(), tree[node * 2 + 1].end(), tree[node].begin());
 	}
 }
 
 ll query(int node, int s, int e, int l, int r, int val) {
-	if (l > e || r < s) {
-		return 0;
-	}
+	if (l > e || r < s) return 0;
 	if (l <= s && e <= r) {
 		// val보다 큰 숫자 세기
 		return tree[node].end() - upper_bound(tree[node].begin(), tree[node].end(), val);
 	}
-	return query(node * 2, s, (s + e) / 2, l, r, val) + query(node * 2 + 1, (s + e) / 2 + 1, e, l, r, val);
+	int mid = (s + e) / 2;
+	return query(node * 2, s, mid, l, r, val) + query(node * 2 + 1, mid + 1, e, l, r, val);
 }
 
 int main() {
@@ -36,14 +38,12 @@ int main() {
 	int n, m;
 	cin >> n;
 
+	v.resize(n + 1);
 	for (int i = 1; i <= n; i++) {
-		cin >> arr[i];
-		update(1, 1, n, i, arr[i]);
+		cin >> v[i];
 	}
 
-	for (int i = 0; i <= 400000; i++) {
-		sort(tree[i].begin(), tree[i].end());
-	}
+	init(1, 1, n);
 
 	ll last_ans = 0;
 	cin >> m;
@@ -54,6 +54,7 @@ int main() {
 		last_ans = query(1, 1, n, a, b, c);
 		cout << last_ans << "\n";
 	}
+
 
 	return 0;
 }
