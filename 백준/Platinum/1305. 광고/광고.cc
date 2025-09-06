1a0b27625212d92a830bac0f0c8b31005b2513c9
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-09-06 17:11:11 +0900

    [Platinum IV] Title: 광고, Time: 8 ms, Memory: 7076 KB -BaekjoonHub

--- 백준/Platinum/1305. 광고/광고.cc
+++ 백준/Platinum/1305. 광고/광고.cc
@@ -1,32 +1,57 @@
 #include <bits/stdc++.h>
-
+#define all(v) v.begin(), v.end()
 using namespace std;
 typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
 
-int j = 0;
+int l;
 
-vector<int> failure(string& s) {
-	vector<int> f(s.size());
-	for (int i = 1; i < s.size(); i++) {
-		while (j > 0 && s[i] != s[j]) j = f[j - 1];
-		if (s[i] == s[j]) f[i] = ++j;
-	}
+vector<int> failure(string& p) {
+    vector<int> f(p.size());
+    int j = 0;
 
-	return f;
+    for (int i = 1; i < p.size(); i++) {
+        while (j > 0 && p[i] != p[j]) j = f[j - 1];
+        if (p[i] == p[j]) f[i] = ++j;
+    }
+
+    return f;
+}
+
+vector<int> kmp(string& t, string& p) {
+    vector<int> f = failure(p);
+    vector<int> res;
+    int j = 0;
+
+    for (int i = 0; i < t.size(); i++) {
+        while (j > 0 && t[i] != p[j]) j = f[j - 1];
+        if (t[i] == p[j]) {
+            if (j == p.size() - 1) {
+                res.push_back(i - p.size() + 1);
+                j = f[j];
+            }
+            else j++;
+        }
+    }
+
+    return res;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	int l;
-	string s;
-	cin >> l;
-	cin >> s;
+    string p;
+    cin >> l;
+    cin >> p;
 
-	vector<int> f = failure(s); // 실패 함수
+    vector<int> f = failure(p);
 
-	cout << l - f[l - 1];
+    cout << l - f[f.size() - 1] << "\n";
 
-	return 0;
+    return 0;
 }
