d7e3b493dd2257d10d49ba0d48a9fe2792bc82ac
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-09-08 16:40:10 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 20 ms, Memory: 3576 KB -BaekjoonHub

--- 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
@@ -9,30 +9,36 @@ typedef pair<ll, ll> pll;
 typedef tuple<ll, ll, ll> tll;
 
 int v, e;
-int visited[10005];
-vector<int> adj[10005], rev_adj[10005];
-vector<int> order;
+bool finished[10005];
+int id[10005], low[10005];
+vector<int> adj[10005];
 vector<vector<int>> res;
+stack<int> st;
+int cnt;
 
-void dfs1(int now) {
-    visited[now] = 1;
+int dfs(int now) {
+    id[now] = low[now] = ++cnt;
+    st.push(now);
 
     for (auto next : adj[now]) {
-        if (!visited[next]) {
-            dfs1(next);
-        }
+        if (!id[next]) low[now] = min(low[now], dfs(next));
+        else if(!finished[next]) low[now] = min(low[now], id[next]);
     }
-    order.push_back(now); // 순서 적기
-}
 
-void dfs2(int now, vector<int>& tmp) {
-    visited[now] = 1;
-    tmp.push_back(now);
-    for (auto next : rev_adj[now]) {
-        if (!visited[next]) {
-            dfs2(next, tmp);
+    if (id[now] == low[now]) {
+        vector<int> tmp;
+        while (1) {
+            int t = st.top();
+            st.pop();
+            finished[t] = true;
+            tmp.push_back(t);
+            if (t == now) break;
         }
+        sort(all(tmp));
+        res.push_back(tmp);
     }
+
+    return low[now];
 }
 
 int main() {
@@ -44,28 +50,13 @@ int main() {
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
-        rev_adj[b].push_back(a);
     }
 
     for (int i = 1; i <= v; i++) {
-        if (!visited[i]) dfs1(i);
+        if (!id[i]) dfs(i);
     }
     
-    reverse(all(order)); // 순서 뒤집기
-    memset(visited, 0, sizeof(visited));
-
-    for (auto it : order) {
-        if (!visited[it]) {
-            vector<int> tmp;
-            dfs2(it, tmp);
-            sort(all(tmp));
-
-            res.push_back(tmp);
-        }
-    }
-
     sort(all(res));
-
     cout << res.size() << "\n";
     for (auto it : res) {
         for (auto it2 : it) {
