d141e764ff2f0e507c0faa2d3e6cb3f39f600084
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-09-09 21:19:33 +0900

    [Silver I] Title: 2-SAT - 1, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11277. 2－SAT － 1/2－SAT － 1.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n, m;
+int bit;
+vector<pii> v;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+    while (m--) {
+        int i, j;
+        cin >> i >> j;
+        v.push_back({ i, j });
+    }
+
+    for (int i = 0; i < (1 << n); i++) {
+        bool flag = true;
+
+        for (auto [a, b] : v) {
+            auto get_val = [&](int x) {
+                if (x > 0) return (i >> (x - 1)) & 1;
+                else return (int)!((i >> (-x - 1)) & 1);
+            };
+
+            int check1 = get_val(a);
+            int check2 = get_val(b);
+
+            if (!check1 && !check2) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) {
+            cout << 1 << "\n";
+            exit(0);
+        }
+    }
+
+    cout << 0 << "\n";
+
+    return 0;
+}
