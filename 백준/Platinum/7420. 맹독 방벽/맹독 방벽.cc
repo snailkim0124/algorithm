b960f2917f60f07ca41f3678522944f25dff9bf1
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-09-14 16:34:03 +0900

    [Platinum IV] Title: 맹독 방벽, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Platinum/7420. 맹독 방벽/맹독 방벽.cc
+++ 백준/Platinum/7420. 맹독 방벽/맹독 방벽.cc
@@ -1,106 +1,86 @@
 #include <bits/stdc++.h>
-#define PI 3.141592
+#define all(v) v.begin(), v.end()
 using namespace std;
 typedef long long ll;
+typedef unsigned long long ull;
 typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
 typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
 
-ll n, l;
-vector<pll> v;
-stack<pll> st;
-vector<pll> v2;
-double sum;
+const double PI = 3.141592;
 
-pll operator-(pll a, pll b) {
-	pll c;
-	c.first = a.first - b.first;
-	c.second = a.second - b.second;
-	return c;
+struct Point {
+    ll x, y;
+    bool operator<(const Point& tmp) const {
+        if (x != tmp.x) return x < tmp.x;
+        return y < tmp.y;
+    }
 };
 
-
-ll dist(pll a, pll b) {
-	return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+ll ccw(Point& a, Point& b, Point& c) {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
 }
 
-ll ccw(pll p1, pll p2, pll p3) {
-	ll s = p1.first * p2.second + p2.first * p3.second + p3.first * p1.second;
-	s -= (p1.second * p2.first + p2.second * p3.first + p3.second * p1.first);
-
-	if (s > 0) return 1;
-	else if (s == 0) return 0;
-	else return -1;
+double dist(Point& a, Point& b) {
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 
-bool cmp(pll a, pll b) {
-	ll cc = ccw(v[0], a, b);
+vector<Point> convexHull(vector<Point>& v) {
+    if (v.size() <= 1) return v;
 
-	// 각도, y, x 작은 순
-	if (cc) {
-		return cc > 0;
-	}
-	else if (a.second != b.second) {
-		return a.second < b.second;
-	}
-	else {
-		return a.first < b.first;
-	}
+    sort(all(v));
+
+    vector<Point> hull;
+    
+    // 하단
+    for (auto p : v) {
+        while (hull.size() >= 2 && ccw(hull[hull.size() - 2], hull.back(), p) <= 0) {
+            hull.pop_back();
+        }
+        hull.push_back(p);
+    }
+    
+    // 상단
+    int sz = hull.size();
+    for (int i = v.size() - 2; i >= 0; i--) {
+        Point p = v[i];
+        while (hull.size() > sz && ccw(hull[hull.size() - 2], hull.back(), p) <= 0) {
+            hull.pop_back();
+        }
+        hull.push_back(p);
+    }
+
+    hull.pop_back(); // 마지막 중복 제거
+
+    return hull;
 }
-
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	cout << fixed;
-	cout.precision(0);
+    int n, l;
+    cin >> n >> l;
+    vector<Point> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].x >> v[i].y;
+    }
 
-	cin >> n >> l;
-	v.resize(n);
-	for (int i = 0; i < n; i++) {
-		cin >> v[i].first >> v[i].second;
-	}
+    vector<Point> hull = convexHull(v);
 
-	for (int i = 1; i < n; i++) {
-		if (v[i].second < v[0].second || (v[i].second == v[0].second && v[i].first < v[0].first)) {
-			swap(v[0].first, v[i].first);
-			swap(v[0].second, v[i].second);
-		}
-	}
+    double ans = 0;
 
-	sort(v.begin() + 1, v.end(), cmp);
+    for (int i = 0; i < hull.size(); i++) {
+        Point a = hull[i];
+        Point b = hull[(i + 1) % hull.size()];
 
-	st.push(v[0]);
-	st.push(v[1]);
+        ans += dist(a, b);
+    }
 
-	for (int i = 2; i < n; i++) {
-		while (st.size() >= 2) {
-			pll top2 = st.top();
-			st.pop();
+    ans += 2 * PI * l;
 
-			pll top1 = st.top();
-			if (ccw(top1, top2, v[i]) > 0) {
-				st.push(top2);
-				break;
-			}
-		}
-		st.push(v[i]);
-	}
+    cout << round(ans) << "\n";
 
-	ll size = st.size();
-	v2.resize(size);
-	for (int i = 0; i < size; i++) {
-		v2[i] = st.top();
-		st.pop();
-	}
-	reverse(v2.begin(), v2.end());
-
-	for (int i = 0; i < size; i++) {
-		// 각 껍질 길이 더하기
-		sum += sqrt(dist(v2[i], v2[(i + 1) % size]));
-	}
-
-	cout << sum + 2 * PI * l; // + 원 둘레
-
-	return 0;
+    return 0;
 }
