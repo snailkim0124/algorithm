118fa712189da51adf49126d71a6a23cee2a1620
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-09-20 22:33:01 +0900

    [Gold IV] Title: 뒤집기 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1464. 뒤집기 3/뒤집기 3.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+string s;
+bool flag;
+deque<char> dq;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> s;
+    
+    for (auto c : s) {
+        if (dq.empty()) {
+            dq.push_back(c);
+        }
+        else if (dq.front() >= c) {
+            dq.push_front(c);
+            // flag = !flag;
+        }
+        else {
+            dq.push_back(c);
+        }
+    }
+    
+    while (!dq.empty()) {
+        cout << dq.front();
+        dq.pop_front();
+    }
+
+    return 0;
+}
