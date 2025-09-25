fa3acccb9a14224500a3141fa0abcc4b229acf03
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-09-25 23:26:07 +0900

    [Silver I] Title: 단절점과 단절선, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.cc
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.cc
@@ -9,37 +9,7 @@ typedef pair<ll, ll> pll;
 typedef tuple<ll, ll, ll> tll;
 
 int n;
-vector<pii> v;
-vector<int> adj[100005];
-int id[100005], low[100005], parent[100005];
-set<pii> ce;
-set<int> cv;
-int cnt;
-
-void dfs(int now) {
-    id[now] = low[now] = ++cnt;
-
-    int child = 0;
-    for (auto next : adj[now]) {
-        if (!id[next]) {
-            child++;
-            parent[next] = now;
-            dfs(next);
-
-            low[now] = min(low[now], low[next]);
-
-            if (low[next] > id[now]) {
-                ce.insert({ min(now, next), max(now, next) });
-            }
-            if (parent[now] == -1 && child > 1) cv.insert(now);
-            if (parent[now] != -1 && low[next] >= id[now]) cv.insert(now);
-
-        }
-        else if (next != parent[now]) {
-            low[now] = min(low[now], id[next]);
-        }
-    }
-}
+int cnt[100005];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -49,15 +19,8 @@ int main() {
     for (int i = 0; i < n - 1; i++) {
         int a, b;
         cin >> a >> b;
-        v.push_back({ min(a, b), max(a, b)});
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-
-    memset(parent, -1, sizeof(parent));
-
-    for (int i = 1; i <= n; i++) {
-        if (!id[i]) dfs(i);
+        cnt[a]++;
+        cnt[b]++;
     }
 
     int q;
@@ -66,10 +29,10 @@ int main() {
         int t, k;
         cin >> t >> k;
         if (t == 1) {
-            cout << (cv.count(k) ? "yes" : "no") << "\n";
+            cout << ((cnt[k] > 1) ? "yes" : "no") << "\n";
         }
         else if (t == 2) {
-            cout << (ce.count(v[k - 1]) ? "yes" : "no") << "\n";
+            cout << "yes\n"; // 트리에서 모든 선이 단절선
         }
     }
 
