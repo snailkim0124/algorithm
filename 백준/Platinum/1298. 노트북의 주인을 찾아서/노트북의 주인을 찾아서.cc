2ac8a7ef7f8ad324f905edb0dbb04c01f9cba525
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-09-29 23:02:17 +0900

    [Platinum IV] Title: 노트북의 주인을 찾아서, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Platinum/1298. 노트북의 주인을 찾아서/노트북의 주인을 찾아서.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n, m, k;
+vector<int> adj[2005];
+int visited[2005], matched[2005];
+
+bool dfs(int now) {
+    for (auto next : adj[now]) {
+        if (visited[next]) continue;
+        visited[next] = 1;
+
+        if (matched[next] == -1 || dfs(matched[next])) {
+            matched[next] = now;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 1; i <= m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+    }
+
+    int cnt = 0;
+    memset(matched, -1, sizeof(matched));
+    for (int i = 1; i <= n; i++) {
+        memset(visited, 0, sizeof(visited));
+        if (dfs(i)) cnt++;
+    }
+
+    cout << cnt << "\n";
+
+    return 0;
+}
