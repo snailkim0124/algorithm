714a168c15006d68bf429e780dd21e7ccb61c692
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-10-05 17:17:46 +0900

    [Platinum III] Title: 열혈강호 4, Time: 216 ms, Memory: 22140 KB -BaekjoonHub

--- 백준/Platinum/11378. 열혈강호 4/열혈강호 4.cc
+++ 백준/Platinum/11378. 열혈강호 4/열혈강호 4.cc
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <limits.h>
 #define all(v) v.begin(), v.end()
 using namespace std;
 typedef long long ll;
@@ -9,61 +10,104 @@ typedef pair<ll, ll> pll;
 typedef tuple<ll, ll, ll> tll;
 
 int n, m, k;
-vector<int> adj[1005];
-int visited[1005], matched[1005];
+vector<int> adj[2005];
+int capacity[2005][2005];
+int parent[2005];
 
-bool dfs(int now) {
-    for (auto next : adj[now]) {
-        if (visited[next]) continue;
-        visited[next] = 1;
+const int INF = 987654321;
 
-        if (matched[next] == -1 || dfs(matched[next])) {
-            matched[next] = now;
-            return true;
-        }
-    }
+int bfs(int s, int e) {
+	memset(parent, -1, sizeof(parent));
 
-    return false;
+	parent[s] = -2;
+	queue<pii> q;
+	q.push({ s, INF });
+
+	while (!q.empty()) {
+		auto [now, flow] = q.front();
+		q.pop();
+
+		for (auto next : adj[now]) {
+			if (parent[next] == -1 && capacity[now][next] > 0) {
+				parent[next] = now;
+				int new_flow = min(flow, capacity[now][next]);
+
+				if (next == e) return new_flow;
+				q.push({ next, new_flow });
+			}
+		}
+	}
+
+	return 0;
+}
+
+int maxflow(int s, int e) {
+	int flow = 0, new_flow;
+
+	while ((new_flow = bfs(s, e))) {
+		flow += new_flow;
+		int now = e;
+		while (now != s) {
+			int prev = parent[now];
+			capacity[prev][now] -= new_flow;
+			capacity[now][prev] += new_flow;
+			now = prev;
+		}
+	}
+
+	return flow;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> m >> k;
-    for (int i = 1; i <= n; i++) {
-        int sz;
-        cin >> sz;
-        for (int j = 0; j < sz; j++) {
-            int num;
-            cin >> num;
-            adj[i].push_back(num);
-        }
-    }
+	cin >> n >> m >> k;
 
-    memset(matched, -1, sizeof(matched));
-    int res = 0;
-    for (int i = 1; i <= n; i++) {
-        memset(visited, 0, sizeof(visited));
-        if (dfs(i)) res++;
-    }
+	int SRC = n + m + 1; // 시작점
+	int P = 0; // 중간점
+	int SINK = n + m + 2; // 도착점
+	
+	// SRC -> 직원
+	for (int i = 1; i <= n; i++) {
+		capacity[SRC][i] = 1;
+		adj[SRC].push_back(i);
+		adj[i].push_back(SRC);
+	}
 
-    int cnt = 0;
-    // 한번 더 돌리기
-    while (1) {
-        bool flag = false;
-        for (int i = 1; i <= n && k > 0; i++) {
-            memset(visited, 0, sizeof(visited));
-            if (dfs(i)) {
-                res++;
-                flag = true;
-                k--;
-            }
-        }
-        if (!flag) break;
-    }
+	// SRC -> P
+	capacity[SRC][0] = k;
+	adj[SRC].push_back(0);
+	adj[0].push_back(SRC);
 
-    cout << res << "\n";
+	// P -> 직원
+	for (int i = 1; i <= n; i++) {
+		capacity[0][i] = k;
+		adj[0].push_back(i);
+		adj[i].push_back(i);
+	}
 
-    return 0;
+	// 직원 -> 업무
+	for (int i = 1; i <= n; i++) {
+		int sz;
+		cin >> sz;
+		for (int j = 0; j < sz; j++) {
+			int num;
+			cin >> num;
+			capacity[i][n + num] = 1;
+			adj[i].push_back(n + num);
+			adj[n + num].push_back(i);
+		}
+	}
+
+	// 업무 -> SINK
+	for (int i = 1; i <= m; i++) {
+		capacity[n + i][SINK] = 1;
+		adj[n + i].push_back(SINK);
+		adj[SINK].push_back(n + i);
+	}
+
+	cout << maxflow(SRC, SINK) << "\n";
+
+	return 0;
 }
