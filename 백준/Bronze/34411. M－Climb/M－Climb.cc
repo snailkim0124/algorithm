377979fd09a0da9545a8751de5e961fded9c7dff
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-10-08 20:49:00 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int m, n, y;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> m >> n >> y;
+	cout << m * n << "\n";
+
+
+	return 0;
+}
