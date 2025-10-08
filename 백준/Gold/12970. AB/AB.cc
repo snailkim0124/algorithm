2a06f8ce27c43f3a00e6caf15bd1a3113befc2ea
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-10-09 01:45:05 +0900

    [Gold IV] Title: AB, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12970. AB/AB.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n, k;
+string ans, tmp;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	ans = "", tmp = "";
+	cin >> n >> k;
+	if ((n / 2) * (n - (n / 2)) < k) {
+		cout << -1 << "\n";
+		exit(0);
+	}
+
+	if (n > k) {
+		for (int i = 0; i < n; i++) {
+			ans += "A";
+		}
+		ans[k] = 'B';
+	}
+	else {
+		for (int i = 0; i < n - (n / 2); i++) {
+			ans += "A";
+		}
+
+		for (int i = n - (n / 2); i < n; i++) {
+			ans += "B";
+		}
+
+		int cnt = (n / 2) * (n - (n / 2));
+		cnt -= k;
+
+		// cout << "cnt : " << cnt << "\n";
+
+		int origin = n - (n / 2) - 1;
+		int idx = origin;
+		int last = n;
+
+		while (cnt--) {
+			if (idx + 1 >= last) {
+				idx = --origin;
+				last--;
+			}
+			swap(ans[idx], ans[idx + 1]);
+			idx++;
+			// cout << "ans : " << ans << "\n";
+		}
+	}
+
+	cout << ans << "\n";
+
+
+	return 0;
+}
