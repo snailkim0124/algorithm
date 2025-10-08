aa108c9f61a91e1eab018aaf981a18b644849995
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-10-09 01:46:39 +0900

    [Gold IV] Title: AB, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/12970. AB/AB.cc
+++ 백준/Gold/12970. AB/AB.cc
@@ -23,39 +23,31 @@ int main() {
 		exit(0);
 	}
 
-	if (n > k) {
-		for (int i = 0; i < n; i++) {
-			ans += "A";
-		}
-		ans[k] = 'B';
+	for (int i = 0; i < n - (n / 2); i++) {
+		ans += "A";
 	}
-	else {
-		for (int i = 0; i < n - (n / 2); i++) {
-			ans += "A";
+
+	for (int i = n - (n / 2); i < n; i++) {
+		ans += "B";
+	}
+
+	int cnt = (n / 2) * (n - (n / 2));
+	cnt -= k;
+
+	// cout << "cnt : " << cnt << "\n";
+
+	int origin = n - (n / 2) - 1;
+	int idx = origin;
+	int last = n;
+
+	while (cnt--) {
+		if (idx + 1 >= last) {
+			idx = --origin;
+			last--;
 		}
-
-		for (int i = n - (n / 2); i < n; i++) {
-			ans += "B";
-		}
-
-		int cnt = (n / 2) * (n - (n / 2));
-		cnt -= k;
-
-		// cout << "cnt : " << cnt << "\n";
-
-		int origin = n - (n / 2) - 1;
-		int idx = origin;
-		int last = n;
-
-		while (cnt--) {
-			if (idx + 1 >= last) {
-				idx = --origin;
-				last--;
-			}
-			swap(ans[idx], ans[idx + 1]);
-			idx++;
-			// cout << "ans : " << ans << "\n";
-		}
+		swap(ans[idx], ans[idx + 1]);
+		idx++;
+		// cout << "ans : " << ans << "\n";
 	}
 
 	cout << ans << "\n";
