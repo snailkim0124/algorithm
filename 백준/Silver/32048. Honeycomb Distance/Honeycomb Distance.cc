2199ee0d1cd4b5f276b4cfaa296041a804e648dc
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-10-11 00:30:22 +0900

    [Silver II] Title: Honeycomb Distance, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/32048. Honeycomb Distance/Honeycomb Distance.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	while (n--) {
+		int x, y;
+		cin >> x >> y;
+		if ((x >= 0 && y >= 0) || (x <= 0 && y <= 0)) {
+			cout << abs(x + y) << "\n";
+		}
+		else {
+			cout << max(abs(x), abs(y)) << "\n";
+		}
+
+	}
+	
+	return 0;
+}
