5b50cb62105a38d816bdfd7a66c4722e677de28d
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-11-06 17:36:08 +0900

    [Platinum I] Title: 아파트 임대, Time: 276 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/5615. 아파트 임대/아파트 임대.cc
+++ 백준/Platinum/5615. 아파트 임대/아파트 임대.cc
@@ -19,36 +19,35 @@ ull divide_pow(ull k, ull x, ull mod) {
 	return res;
 }
 
-bool miller(ull d, ull n, ull a) {
-	ull x = divide_pow(a, d, n);
-
-	if (x == 1 || x == n - 1) return true;
-	while (d != n - 1) {
-		x = (x * x) % n;
-		d <<= 1;
-		// 합성수
-		if (x == 1) return false;
-		// 소수일 수 있음
-		if (x == n - 1) return true;
-	}
-
-	return false;
-}
-
-bool isprime(ll n) {
+bool miller(ll n) {
 	if (n < 2) return false;
 
 	for (ull p : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
 		if (n % p == 0) return n == p;
 	}
 
-	ull d = n - 1;
-	while ((d & 1) == 0) d >>= 1;
+	ull d = n - 1, s = 0;
+	while ((d & 1) == 0) {
+		d >>= 1;
+		s++;
+	}
 
 	// 확률적으로 접근
 	for (ull a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
 		if (a % n == 0) continue;
-		if (!miller(d, n, a)) return false;
+		ull x = divide_pow(a, d, n);
+
+		if (x == 1 || x == n - 1) continue;
+		bool flag = true;
+		for (int i = 1; i < s; i++) {
+			x = (x * x) % n;
+			if (x == n - 1) {
+				flag = false;
+				break;
+			}
+		}
+
+		if (flag) return false;
 	}
 
 	return true;
@@ -64,7 +63,7 @@ int main() {
 		ull s;
 		cin >> s;
 		// 2 * s + 1이 소수면 없음
-		if (isprime(2 * s + 1)) cnt++;
+		if (miller(2 * s + 1)) cnt++;
 	}
 	
 	cout << cnt << "\n";
