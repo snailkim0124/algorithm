a9768b4dd408759772bff66382bb5f7dd9418c3c
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-11-08 01:45:00 +0900

    [Silver I] Title: 카잉 달력, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.cc
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -10,6 +10,21 @@ typedef tuple<ll, ll, ll> tll;
 
 int t;
 
+tll ext_gcd(ll a, ll b) {
+	if (b == 0) return { a, 1, 0 };
+	auto [g, x1, y1] = ext_gcd(b, a % b);
+	return { g, y1, x1 - (a / b) * y1 };
+}
+
+// 모듈러 역원
+ll modinv(ll a, ll mod) {
+	auto [g, x, y] = ext_gcd(a, mod);
+	if (g != 1) return -1; // 역원 없음
+	x %= mod;
+	if (x < 0) x += mod;
+	return x;
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -21,21 +36,37 @@ int main() {
 		ll m, n, x, y;
 		cin >> m >> n >> x >> y;
 
-		ll ans = -1;
+		x--; y--;
+			
+		ll g = gcd(m, n);
 
-		if (y == n) y = 0;
-
-		for (ll i = x; i <= m * n; i += m) {
-			if (i % n == y) {
-				ans = i;
-				break;
-			}
+		if ((y - x) % g != 0) {
+			cout << -1 << "\n";
+			continue;
 		}
 
-		cout << ans << "\n";
+		// 중국인 나머지 정리
+		ll lcm = m * n / g;
+
+		ll d = (y - x) / g;
+		ll mg = m / g;
+		ll ng = n / g;
+
+		ll inv = modinv(mg, ng);
+		if (inv == -1) {
+			cout << -1 << "\n";
+			continue;
+		}
+
+		ll tmp = (d % ng) * inv % ng;
+		ll ans = x + tmp * m;
+
+		ans = ((ans % lcm) + lcm) % lcm; // 보정
+		
+		cout << ans + 1 << "\n";
 
 	}
 
-	
+
 	return 0;
 }
