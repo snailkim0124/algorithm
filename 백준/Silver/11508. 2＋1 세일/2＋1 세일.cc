5e6cb38371b1bc589b781e7fbadde4c2ab3e6e39
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-11-20 23:02:51 +0900

    [Silver IV] Title: 2+1 세일, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+ll n;
+ll arr[100005];
+ll sum;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
+
+	sort(arr, arr + n, greater<>());
+	
+	for (int i = 0; i < n; i++) {
+		if ((i + 1) % 3 == 0) {
+			sum -= arr[i];
+		}
+	}
+
+	cout << sum << "\n";
+	
+
+	return 0;
+}
