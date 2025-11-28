0d466a9302ef58b7ed82b133570bc5116ee1149f
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-11-28 22:21:18 +0900

    [Bronze IV] Title: ПЧЕЛИЧКАТА МАЯ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24365. ПЧЕЛИЧКАТА МАЯ/ПЧЕЛИЧКАТА МАЯ.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int a, b, c;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> a >> b >> c;
+	cout << max({ a,b,c }) - min({ a,b,c }) << "\n";
+	
+	
+	return 0;
+}
