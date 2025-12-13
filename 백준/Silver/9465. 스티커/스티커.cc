d73a977e4b2b1fff76c37d11382238518466a3f6
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-12-13 23:44:35 +0900

    [Silver I] Title: 스티커, Time: 80 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,41 +1,46 @@
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <string>
-
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
 using namespace std;
-int t;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int tt;
 int n;
-int a[2][100004];
-int dp[2][100004];
+int arr[2][200005], dp[200005][3];
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		fill(a[0], a[0] + 2 * 100004, 0);
-		fill(dp[0], dp[0] + 2 * 100004, 0);
-		long long int res = 0;
+	cin >> tt;
+	while (tt--) {
+		memset(arr, 0, sizeof(arr));
+		memset(dp, 0, sizeof(dp));
+
 		cin >> n;
-		for (int j = 0; j < 2; j++) {
-			for (int k = 1; k <= n; k++) {
-				cin >> a[j][k];
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> arr[i][j];
 			}
 		}
+
+		dp[0][1] = arr[0][0];
+		dp[0][2] = arr[1][0];
 		dp[0][0] = 0;
-		dp[1][0] = 0;
-		dp[0][1] = a[0][1];
-		dp[1][1] = a[1][1];
-		for (int j = 2; j <= n; j++) {
-			dp[0][j] = max(dp[1][j - 1], dp[1][j-2]) + a[0][j];
-			dp[1][j] = max(dp[0][j - 1], dp[0][j-2]) + a[1][j];
+
+		for (int j = 1; j < n; j++) {
+			dp[j][1] = max(dp[j - 1][2], dp[j - 1][0]) + arr[0][j]; // 위
+			dp[j][2] = max(dp[j - 1][1], dp[j - 1][0]) + arr[1][j]; // 아래
+			dp[j][0] = max({ dp[j - 1][0], dp[j - 1][1], dp[j - 1][2] });
 		}
-		res = max(dp[0][n], dp[1][n]);
-		cout << res << "\n";
+
+		cout << max({ dp[n - 1][0], dp[n - 1][1], dp[n - 1][2] }) << "\n";
 	}
+	
+
 	return 0;
 }
