1cc87ef97ca25992d7121b0f46acc0c1fda0f2b9
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-12-14 22:14:47 +0900

    [Platinum IV] Title: 님 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/11868. 님 게임 2/님 게임 2.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	int x = 0;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		x ^= num;
+	}
+
+	cout << (x ? "koosaga" : "cubelover") << "\n";
+	
+	return 0;
+}
