eee0ca13f4843c41f8ef6e824d138bb93683c347
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2025-12-20 19:40:55 +0900

    [Gold III] Title: 가장 오래 걸리는 스도쿠, Time: 0 ms, Memory: 2020 KB, Score: 82 point -BaekjoonHub

---
+++ 백준/Gold/12095. 가장 오래 걸리는 스도쿠/가장 오래 걸리는 스도쿠.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int arr[15][15] = {
+	{ 0, 3, 5, 4, 6, 9, 2, 7, 8 },
+	{ 7, 8, 2, 1, 0, 5, 6, 0, 9 },
+	{ 0, 6, 0, 2, 7, 8, 1, 3, 5 },
+	{ 3, 2, 1, 0, 4, 6, 8, 9, 7 },
+	{ 8, 0, 4, 9, 1, 3, 5, 0, 6 },
+	{ 5, 9, 6, 8, 2, 0, 4, 1, 3 },
+	{ 9, 1, 7, 6, 5, 2, 0, 8, 0 },
+	{ 6, 0, 3, 7, 0, 1, 9, 5, 2 },
+	{ 2, 5, 8, 3, 9, 4, 7, 6, 0 },
+};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	
+
+	return 0;
+}
