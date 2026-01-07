15d464c16781e8185ff3cf99155add8305543593
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-01-07 21:50:47 +0900

    [Platinum III] Title: 최장 공통 부분 문자열, Time: 192 ms, Memory: 6836 KB -BaekjoonHub

---
+++ 백준/Platinum/9249. 최장 공통 부분 문자열/최장 공통 부분 문자열.cc
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+vector<int> v, rnk;
+int n, k;
+
+bool cmp(int i, int j) {
+	if (rnk[i] != rnk[j]) return rnk[i] < rnk[j];
+
+	int ri = (i + k < n ? rnk[i + k] : -1);
+	int rj = (j + k < n ? rnk[j + k] : -1);
+
+	return ri < rj;
+}
+
+vector<int> sa_func(string s) {
+	n = s.size();
+	vector<int> sa(n);
+	rnk.resize(n, 0);
+
+	for (int i = 0; i < n; i++) {
+		sa[i] = i;
+		rnk[i] = s[i];
+	}
+
+	vector<int> tmp(n);
+	// 효율적으로 비교하기 위해 2배씩
+	for (k = 1;; k <<= 1) {
+		sort(all(sa), cmp);
+
+		tmp[sa[0]] = 0;
+		// 이전 suffix 배열 요소 비교
+		for (int i = 1; i < n; i++) {
+			tmp[sa[i]] = tmp[sa[i - 1]] + (cmp(sa[i - 1], sa[i]) ? 1 : 0);
+		}
+
+		rnk = tmp;
+		if (rnk[sa[n - 1]] == n - 1) break;
+	}
+
+	return sa;
+}
+
+vector<int> lcp_func(string s, vector<int> sa) {
+	int n = s.size();
+	vector<int> lcp(n), rank(n);
+	
+	// suffix 배열에서 몇번째?
+	for (int i = 0; i < n; i++) {
+		rank[sa[i]] = i;
+	}
+
+	int idx = 0;
+	for (int i = 0; i < n; i++) {
+		if (rank[i] > 0) {
+			// 바로 이전 suffix
+			int j = sa[rank[i] - 1];
+			while (i + idx < n && j + idx < n && s[i + idx] == s[j + idx]) {
+				idx++;
+			}
+
+			lcp[rank[i]] = idx;
+
+			if (idx > 0) idx--;
+		}
+	}
+
+	return lcp;
+
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string a, b;
+	cin >> a >> b;
+	if (a.size() < b.size()) swap(a, b);
+	string tmp = a + '#' + b;
+	int shap = a.size(); // 구분용
+
+	vector<int> sa = sa_func(tmp);
+
+	vector<int> lcp = lcp_func(tmp, sa);
+
+	//for (int i = 1; i < sa.size(); i++) {
+	//	cout << i << " : " << sa[i] << " : " << tmp.substr(sa[i]) << "\n";
+	//}
+
+	int mx = 0;
+	int idx = -1;
+	for (int i = 1; i < lcp.size(); i++) {
+		// 두 접미사 위치가 shap을 기점으로 양쪽에 있어야 됨
+		if ((sa[i] < shap && sa[i - 1] > shap) || (sa[i] > shap && sa[i - 1] < shap)) {
+			if (mx < lcp[i]) {
+				mx = lcp[i];
+				idx = sa[i];
+			}
+		}
+		// cout << i << " : " << sa[i]<< "\n";
+	}
+
+	cout << mx << "\n";
+	cout << tmp.substr(idx, mx) << "\n";
+
+	//cout << "\n";
+
+	return 0;
+}
