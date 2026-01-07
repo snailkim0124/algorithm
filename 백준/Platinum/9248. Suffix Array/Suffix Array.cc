7ce94ad36b65f47414e1698fe033171005744be8
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-01-07 16:20:41 +0900

    [Platinum III] Title: Suffix Array, Time: 556 ms, Memory: 12920 KB -BaekjoonHub

---
+++ 백준/Platinum/9248. Suffix Array/Suffix Array.cc
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+vector<int> v, rnk;
+int n, k;
+
+bool cmp(int i, int j) {
+	if (rnk[i] != rnk[j]) return rnk[i] < rnk[j];
+
+	int ri = (i + k < n ? rnk[i + k] : -1);
+	int rj = (j + k < n ? rnk[j + k] : -1);
+
+	return ri < rj;
+}
+
+vector<int> sa_func(string s) {
+	n = s.size();
+	vector<int> sa(n);
+	rnk.resize(n, 0);
+
+	for (int i = 0; i < n; i++) {
+		sa[i] = i;
+		rnk[i] = s[i];
+	}
+
+	vector<int> tmp(n);
+	// 효율적으로 비교하기 위해 2배씩
+	for (k = 1;; k <<= 1) {
+		sort(all(sa), cmp);
+
+		tmp[sa[0]] = 0;
+		// 이전 suffix 배열 요소 비교
+		for (int i = 1; i < n; i++) {
+			tmp[sa[i]] = tmp[sa[i - 1]] + (cmp(sa[i - 1], sa[i]) ? 1 : 0);
+		}
+
+		rnk = tmp;
+		if (rnk[sa[n - 1]] == n - 1) break;
+	}
+
+	return sa;
+}
+
+vector<int> lcp_func(string s, vector<int> sa) {
+	int n = s.size();
+	vector<int> lcp(n), rank(n);
+	
+	// suffix 배열에서 몇번째?
+	for (int i = 0; i < n; i++) {
+		rank[sa[i]] = i;
+	}
+
+	int idx = 0;
+	for (int i = 0; i < n; i++) {
+		if (rank[i] > 0) {
+			// 바로 이전 suffix
+			int j = sa[rank[i] - 1];
+			while (i + idx < n && j + idx < n && s[i + idx] == s[j + idx]) {
+				idx++;
+			}
+
+			lcp[rank[i]] = idx;
+
+			if (idx > 0) idx--;
+		}
+	}
+
+	return lcp;
+
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string s;
+	cin >> s;
+
+	// 접미사 정렬
+	vector<int> sa = sa_func(s);
+	for (int i = 0; i < sa.size(); i++) {
+		cout << sa[i] + 1 << " ";
+	}
+	cout << "\n";
+
+	// lcp
+	vector<int> lcp = lcp_func(s, sa);
+	cout << "x ";
+	for (int i = 1; i < lcp.size(); i++) {
+		cout << lcp[i] << " ";
+	}
+	cout << "\n";
+
+
+	return 0;
+}
