dbeec2338b68a26c4f4d150e5c5b7028efc3379b
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-01-15 23:19:23 +0900

    [Gold I] Title: 수열과 쿼리 37, Time: 68 ms, Memory: 4872 KB -BaekjoonHub

--- 백준/Gold/18436. 수열과 쿼리 37/수열과 쿼리 37.cc
+++ 백준/Gold/18436. 수열과 쿼리 37/수열과 쿼리 37.cc
@@ -8,18 +8,18 @@ typedef tuple<int, int, int> tii;
 typedef pair<ll, ll> pll;
 typedef tuple<ll, ll, ll> tll;
 
-vector<ll> odd_tree, even_tree;
-vector<ll> odd, even;
+vector<ll> tree;
+vector<ll> a;
 
-ll init(vector<ll>& tree, vector<ll>& a, int node, int s, int e) {
+ll init(int node, int s, int e) {
 	if (s == e) return tree[node] = a[s];
 	else {
 		int mid = (s + e) / 2;
-		return tree[node] = init(tree, a, node * 2, s, mid) + init(tree, a, node * 2 + 1, mid + 1, e);
+		return tree[node] = init(node * 2, s, mid) + init(node * 2 + 1, mid + 1, e);
 	}
 }
 
-void update(vector<ll>& tree, int node, int s, int e, int idx, ll val) {
+void update(int node, int s, int e, int idx, ll val) {
 	if (idx < s || idx > e) return;
 	if (s == e) {
 		tree[node] = val;
@@ -27,17 +27,17 @@ void update(vector<ll>& tree, int node, int s, int e, int idx, ll val) {
 	}
 
 	int mid = (s + e) / 2;
-	update(tree, node * 2, s, mid, idx, val);
-	update(tree, node * 2 + 1, mid + 1, e, idx, val);
+	update(node * 2, s, mid, idx, val);
+	update(node * 2 + 1, mid + 1, e, idx, val);
 	tree[node] = tree[node * 2] + tree[node * 2 + 1];
 }
 
-ll query(vector<ll>& tree, int node, int s, int e, int l, int r) {
+ll query(int node, int s, int e, int l, int r) {
 	if (l > e || r < s) return 0; // 범위 밖
 	if (l <= s && e <= r) return tree[node];
 
 	int mid = (s + e) / 2;
-	return query(tree, node * 2, s, mid, l, r) + query(tree, node * 2 + 1, mid + 1, e, l, r);
+	return query(node * 2, s, mid, l, r) + query(node * 2 + 1, mid + 1, e, l, r);
 }
 
 int main() {
@@ -48,21 +48,16 @@ int main() {
 	cin >> n;
 	int h = (int)ceil(log2(n));
 	int tree_size = (1 << (h + 1));
-	odd_tree.resize(tree_size);
-	even_tree.resize(tree_size);
+	tree.resize(tree_size);
 
-	odd.resize(n + 1, 0);
-	even.resize(n + 1, 0);
+	a.resize(n + 1, 0);
 
 	for (int i = 1; i <= n; i++) {
-		int num;
-		cin >> num;
-		if (num % 2 == 1) odd[i]++;
-		else even[i]++;
+		cin >> a[i];
+		a[i] %= 2;
 	}
 
-	init(odd_tree, odd, 1, 1, n);
-	init(even_tree, even, 1, 1, n);
+	init(1, 1, n);
 
 	int m;
 	cin >> m;
@@ -72,28 +67,18 @@ int main() {
 		if (cmd == 1) {
 			ll i, x;
 			cin >> i >> x;
-			if (odd[i] && x % 2 == 0) {
-				odd[i]--;
-				even[i]++;
-				update(odd_tree, 1, 1, n, i, 0);
-				update(even_tree, 1, 1, n, i, 1);
-			}
-			else if (even[i] && x % 2 == 1) {
-				even[i]--;
-				odd[i]++;
-				update(even_tree, 1, 1, n, i, 0);
-				update(odd_tree, 1, 1, n, i, 1);
-			}
+			update(1, 1, n, i, x % 2);
 		}
 		else if (cmd == 2) {
 			ll l, r;
 			cin >> l >> r;
-			cout << query(even_tree, 1, 1, n, l, r) << "\n";
+			ll sz = r - l + 1;
+			cout << sz - query(1, 1, n, l, r) << "\n";
 		}
 		else if (cmd == 3) {
 			ll l, r;
 			cin >> l >> r;
-			cout << query(odd_tree, 1, 1, n, l, r) << "\n";
+			cout << query(1, 1, n, l, r) << "\n";
 		}
 	}
 
