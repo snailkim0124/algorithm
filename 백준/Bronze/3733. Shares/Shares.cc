6ddd063008c459798ce3dfb6aae6b109c9e78ed8
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-02-01 21:53:33 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n, s;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	while (cin >> n >> s) {
+		cout << s / (n + 1) << "\n";
+	}
+	
+	
+	return 0;
+}
