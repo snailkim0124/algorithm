d900cd856ae444a46613d69201de0f1ae7e67ada
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-02-02 20:00:38 +0900

    [Gold II] Title: 서로소의 개수, Time: 12 ms, Memory: 44992 KB -BaekjoonHub

--- 백준/Gold/1750. 서로소의 개수/서로소의 개수.cc
+++ 백준/Gold/1750. 서로소의 개수/서로소의 개수.cc
@@ -9,35 +9,32 @@ typedef pair<ll, ll> pll;
 typedef tuple<ll, ll, ll> tll;
 
 const ll mod = 10'000'003;
-ll n;
+ll n, mx;
 ll arr[55];
 ll dp[55][100005];
-
-ll go(ll idx, ll g) {
-	if (idx == n) return (g == 1);
-	if (dp[idx][g] != -1) return dp[idx][g] % mod;
-
-	ll res = 0;
-	res = (res + go(idx + 1, gcd(g, arr[idx]))) % mod;
-	// 스킵
-	res = (res + go(idx + 1, g)) % mod;
-
-	return dp[idx][g] = res % mod;
-
-}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
+		mx = max(mx, arr[i]);
 	}
 
-	memset(dp, -1, sizeof(dp));
-	cout << go(0, 0) << "\n";
+	for (int i = 1; i <= n; i++) {
+		dp[i][arr[i]] = (dp[i][arr[i]] + 1) % mod; // 자기자신
 
+		for (int g = 1; g <= mx; g++) {
+			if (!dp[i - 1][g]) continue;
+			dp[i][g] = (dp[i][g] + dp[i - 1][g]) % mod;
+			dp[i][gcd(g, arr[i])] = (dp[i][gcd(g, arr[i])] + dp[i - 1][g]) % mod;
+		}
+	}
+
+	cout << dp[n][1] % mod << "\n";
+	
 	return 0;
 }
 
