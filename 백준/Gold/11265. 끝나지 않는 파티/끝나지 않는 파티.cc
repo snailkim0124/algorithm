a0269207f54c6992b06d4a7632dbecea154d1527
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-02-09 14:22:24 +0900

    [Gold IV] Title: 끝나지 않는 파티, Time: 40 ms, Memory: 3016 KB -BaekjoonHub

---
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n;
+int dist[505][505];
+int m;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> dist[i][j];
+		}
+	}
+
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+			}
+		}
+	}
+
+	while (m--) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		if (dist[a][b] <= c) {
+			cout << "Enjoy other party\n";
+		}
+		else {
+			cout << "Stay here\n";
+		}
+	}
+	
+
+	return 0;
+}
