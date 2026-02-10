0f7d342f8a468a9f93d34e00bd428af465823582
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-02-10 22:13:48 +0900

    [Platinum V] Title: 순열복원, Time: 28 ms, Memory: 5656 KB -BaekjoonHub

--- 백준/Platinum/1777. 순열복원/순열복원.cc
+++ 백준/Platinum/1777. 순열복원/순열복원.cc
@@ -19,27 +19,21 @@ ll init(int node, int s, int e) {
 	}
 }
 
-void update(int node, int s, int e, int idx, ll val) {
-	if (idx < s || idx > e) return;
-	if (s == e) {
-		tree[node] = val;
-		return;
-	}
+void update(int node, int s, int e, int idx) {
+	tree[node]--; // 모든 경로에 있는 거 없애기
+	if (s == e) return;
 
 	int mid = (s + e) / 2;
-	update(node * 2, s, mid, idx, val);
-	update(node * 2 + 1, mid + 1, e, idx, val);
-	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	if (idx <= mid) update(node * 2, s, mid, idx);
+	else update(node * 2 + 1, mid + 1, e, idx);
 }
 
-ll query(int node, int s, int e, int l, int r) {
-	if (l > e || r < s) return 0; // 범위 밖
-	if (l <= s && e <= r) return tree[node];
-
+ll query(int node, int s, int e, int val) {
+	if (s == e) return s;
 	int mid = (s + e) / 2;
-	return query(node * 2, s, mid, l, r) + query(node * 2 + 1, mid + 1, e, l, r);
+	if (tree[node * 2] >= val) return query(node * 2, s, mid, val);
+	else return query(node * 2 + 1, mid + 1, e, val - tree[node * 2]);
 }
-
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -62,24 +56,11 @@ int main() {
 
 	// 역순으로 쓴거는 없애기
 	for (int i = n; i >= 1; i--) {
-		int tmp = query(1, 1, n, 1, n) - v[i]; // 순서 찾기
-
-		int left = 1;
-		int right = n;
-		int idx = 0;
-
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			
-			if (query(1, 1, n, 1, mid) >= tmp) {
-				idx = mid;
-				right = mid - 1;
-			}
-			else left = mid + 1;
-		}
-
+		int tmp = tree[1];
+		int idx = query(1, 1, n, tmp - v[i]);
 		ans[idx] = i;
-		update(1, 1, n, idx, 0);
+
+		update(1, 1, n, idx);
 	}
 
 	for (int i = 1; i <= n; i++) {
