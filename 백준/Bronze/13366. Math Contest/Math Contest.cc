763fb7e7945ac5909ec2fb71c412a9bd7377ed99
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-02-15 20:05:22 +0900

    [Bronze III] Title: Math Contest, Time: 196 ms, Memory: 2552 KB -BaekjoonHub

---
+++ 백준/Bronze/13366. Math Contest/Math Contest.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+void solve() {
+	string x;
+	cin >> x;
+
+	ll sum = 0;
+	for (auto it : x) {
+		sum += it - '0';
+	}
+
+	if (sum % 9 == 0) cout << "YES\n";
+	else cout << "NO\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		solve();
+	}
+
+	return 0;
+}
