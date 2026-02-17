bde91875fb35bd781099c2355c1d5beecc0c76db
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-02-17 21:46:51 +0900

    [Gold II] Title: $A$의 배수, Time: 256 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/34124. $A$의 배수/$A$의 배수.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+void solve() {
+	int n, a;
+	cin >> n >> a;
+	
+	if (n % 2 == 1) {
+		cout << "O\n";
+	}
+	else {
+		if (n == 2) cout << "O\n";
+		else cout << "I\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		solve();
+	}
+	return 0;
+}
