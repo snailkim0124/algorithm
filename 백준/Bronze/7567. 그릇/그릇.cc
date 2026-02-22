b0c99cc333fefb8f6d409727a76ccce435e08649
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-02-22 22:09:28 +0900

    [Bronze II] Title: 그릇, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/7567. 그릇/그릇.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+string s;
+int sum;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> s;
+	sum += 10;
+	char prev = s[0];
+
+	for (int i = 1; i < s.size(); i++) {
+		if (prev == s[i]) sum += 5;
+		else {
+			sum += 10;
+			prev = s[i];
+		}
+	}
+
+	cout << sum << "\n";
+
+	return 0;
+}
