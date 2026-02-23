ca090312c7076556562f5f2c9fe658db625680fc
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-02-23 19:52:13 +0900

    [Gold V] Title: 으악그래프, Time: 152 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/35112. 으악그래프/으악그래프.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n, m;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		cin >> u >> v;
+	}
+
+	if (m <= n) cout << "Yes\n";
+	else cout << "No\n";
+	
+	
+    
+	return 0;
+}
