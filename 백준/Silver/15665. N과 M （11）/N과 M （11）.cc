92da34bc1b9794df8b4aa99ed10f687cee32a5fb
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-03-22 21:00:25 +0900

    [Silver II] Title: N과 M (11), Time: 116 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n, m;
+int arr[10];
+int res[10];
+int visited[10];
+
+void go(int depth) {
+	int last = 0;
+
+	if (depth == m) {
+		for (int i = 0; i < m; i++) {
+			cout << res[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (last != arr[i]) {
+			last = res[depth] = arr[i];
+			go(depth + 1); // 다음 칸을 채우러 감
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	go(0);
+	
+	return 0;
+}
