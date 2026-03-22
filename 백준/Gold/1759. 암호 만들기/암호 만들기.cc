86c9aef05b0df10878a961177b6783c6e4d8408d
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-03-22 21:28:36 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -1,47 +1,58 @@
 #include <bits/stdc++.h>
-
+#define all(v) v.begin(), v.end()
 using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
 
-int L, C;
-vector<char> a;
+int l, c;
+vector<char> v;
 vector<char> res;
+int visited[20];
 
-void dfs(int start) {
-	if ((int)res.size() == L) {
-		int aeiou = 0;
-		for (int i = 0; i < L; i++) {
-			if (res[i] == 'a' || res[i] == 'e' || res[i] == 'i' || res[i] == 'o' || res[i] == 'u') aeiou++;
+void go(int idx) {
+	if ((int)res.size() == l) {
+		int moum = 0;
+		for (int i = 0; i < l; i++) {
+			if (res[i] == 'a' || res[i] == 'e' || res[i] == 'i' || res[i] == 'o' || res[i] == 'u') moum++;
 		}
-		if (aeiou >= 1 && L - aeiou >= 2) { // 모음, 자음 체크
-			for (int i = 0; i < L; i++) {
+
+		int jaum = l - moum;
+		if (moum >= 1 && jaum >= 2) {
+			for (int i = 0; i < l; i++) {
 				cout << res[i];
 			}
 			cout << "\n";
 		}
+
 		return;
 	}
-	else {
-		for (int i = start; i < C; i++) {
-			res.push_back(a[i]);
-			dfs(i + 1);
-			res.pop_back();
-		}
+
+	for (int i = idx; i < c; i++) {
+		res.push_back(v[i]);
+		go(i + 1);
+		res.pop_back();
 	}
 }
+
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> L >> C;
-	for (int i = 0; i < C; i++) {
-		char alpha;
-		cin >> alpha;
-		a.push_back(alpha);
+	cin >> l >> c;
+	for (int i = 0; i < c; i++) {
+		char al;
+		cin >> al;
+		v.push_back(al);
 	}
 
-	sort(a.begin(), a.end());
-	dfs(0);
-	
+	sort(all(v));
+
+	go(0);
+
 	return 0;
 }
