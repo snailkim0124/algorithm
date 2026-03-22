cf79934806fd52c3cdaf1721eea6fcdf227b4d67
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-03-22 20:55:24 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,55 +1,52 @@
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <cmath>
-#include <string>
-#include <stack>
-#include <deque>
-#include <cmath>
-#include <tuple>
-
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
 using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
 
 int n, m;
-int res[10];
 int arr[10];
-int check[10];
+int res[10];
+int visited[10];
 
-void dfs(int depth) {
-	int num = 0;
+void go(int depth) {
+	int now = 0;
+
 	if (depth == m) {
 		for (int i = 0; i < m; i++) {
 			cout << res[i] << " ";
 		}
 		cout << "\n";
+		return;
 	}
 
-	else {
-		for (int i = 0; i < n; i++) {
-			if (check[i] == 0) {
-				if (num == arr[i]) continue;
-				res[depth] = arr[i];
-				num = res[depth];
-				check[i] = 1;
-				dfs(depth + 1);
-				check[i] = 0;
-			}
+	for (int i = 0; i < n; i++) {
+		if (!visited[i] && now != arr[i]) {
+			visited[i] = 1;
+			now = res[depth] = arr[i];
+			go(depth + 1); // 다음 칸을 채우러 감
+
+			visited[i] = 0;
 		}
 	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
+
 	sort(arr, arr + n);
 
-	dfs(0);
+	go(0);
+	
 	return 0;
 }
