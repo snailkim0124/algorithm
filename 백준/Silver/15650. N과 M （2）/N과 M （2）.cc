7981c31783d0caebadd07360a9ca9a565a1f3878
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-03-24 20:01:08 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+int n, m;
+int res[10];
+int visited[10];
+
+void go(int depth, int prev) {
+	if (depth == m) {
+		for (int i = 0; i < m; i++) {
+			cout << res[i] << " ";
+		}
+		cout << "\n";
+
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (prev < i && !visited[i]) {
+			visited[i] = true; // 이 숫자는 이미 씀
+			res[depth] = i;
+
+			go(depth + 1, i); // 다음 칸을 채우러 감
+
+			visited[i] = false; // 다음 경로를 위해 내가 남긴 표시를 지우기
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+	go(0, 0);
+
+	return 0;
+}
