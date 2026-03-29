39f4f8ad59607822ce7769360472af331d7cc782
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-03-29 23:39:44 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2316 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,74 +1,83 @@
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <algorithm>
-#include <queue>
-
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
 using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
 int n, m, v;
+vector<int> adj[1005];
+int visited[1005];
+vector<int> dpath, bpath;
 
-vector<int> adj[100001];
-int visited_bfs[100001];
-int visited_dfs[100001];
-vector<int> result_bfs;
-vector<int> result_dfs;
-int cnt_dfs;
-int cnt_bfs;
+void dfs(int now) {
+	visited[now] = 1;
+	dpath.push_back(now);
 
-void dfs(int r) {
-	visited_dfs[r] = 1;
-	result_dfs.push_back(r);
-
-	for (int i = 0; i < adj[r].size(); i++) {
-		if(!visited_dfs[adj[r][i]])	dfs(adj[r][i]);
+	for (auto next : adj[now]) {
+		if (!visited[next]) {
+			dfs(next);
+		}
 	}
-	return;
 }
 
-
-void bfs(int r) {
+void bfs(int now) {
 	queue<int> q;
-	q.push(r);
-	visited_bfs[r] = 1;
-	while (!q.empty()) {
-		int here = q.front();
-		q.pop();
-		result_bfs.push_back(here);
+	visited[now] = 1;
+	q.push(now);
 
-		for (int i = 0; i < adj[here].size(); i++) {
-			if (!visited_bfs[adj[here][i]]) {
-				q.push(adj[here][i]);
-				visited_bfs[adj[here][i]] = 1;
+	bpath.push_back(now);
+
+	while (!q.empty()) {
+		auto now = q.front();
+		q.pop();
+
+		for (auto next : adj[now]) {
+			if (!visited[next]) {
+				visited[next] = 1;
+				bpath.push_back(next);
+
+				q.push(next);
 			}
 		}
 	}
-	return;
 }
-
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-
+	
 	cin >> n >> m >> v;
 	for (int i = 0; i < m; i++) {
-		int x, y;
-		cin >> x >> y;
-		adj[x].push_back(y);
-		adj[y].push_back(x);
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
 	}
+
 	for (int i = 1; i <= n; i++) {
-		sort(adj[i].begin(), adj[i].end());
+		sort(all(adj[i]));
 	}
+
 	dfs(v);
+	
+	memset(visited, 0, sizeof(visited));
+
 	bfs(v);
-	for (int i = 0; i < result_dfs.size(); i++) {
-		cout << result_dfs[i] << " ";
+
+	for (auto it : dpath) {
+		cout << it << " ";
 	}
 	cout << "\n";
-	for (int i = 0; i < result_bfs.size(); i++) {
-		cout << result_bfs[i] << " ";
+
+	for (auto it : bpath) {
+		cout << it << " ";
 	}
+	cout << "\n";
+
 
 	return 0;
 }
