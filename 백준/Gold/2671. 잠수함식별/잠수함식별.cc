94eb8f8f7028d55271b92348dc01d2497bc42fdc
Minhwan Kim <128965735+snailkim0124@users.noreply.github.com>
2026-04-25 16:41:18 +0900

    [Gold V] Title: 잠수함식별, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <regex>
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tll;
+
+string s;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	regex e("(100+1+|01)+");
+	
+	cin >> s;
+
+	if (regex_match(s, e)) {
+		cout << "SUBMARINE\n";
+	}
+	else cout << "NOISE\n";
+	
+
+	return 0;
+}
